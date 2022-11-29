ef0686a221e45e3d6941e8ba760ca83629da32bd
DarthofLuke <60023923+DarthofLuke@users.noreply.github.com>
2022-11-29 14:38:44 -0500

    Changed to use Typescript (#27)

--- sudokuruweb/src/app/App.tsx
+++ sudokuruweb/src/app/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+//@ts-ignore
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>
         {`This is ${process.env.REACT_APP_NAME}`}
