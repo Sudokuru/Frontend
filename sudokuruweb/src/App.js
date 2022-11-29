7db82a37073d9dc047a55d23068a0d16d8e38b9e
Arthur Upfield <cereusware@icloud.com>
2022-11-28 22:36:48 -0500

    Docker Config Added

--- sudokuruweb/src/App.js
+++ sudokuruweb/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+        {`This is ${process.env.REACT_APP_NAME}`}
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
