a055d0f5ac7ef1a1b0f8b2a97749913029f1ae20
Galla <gallant.thomas@outlook.com>
2022-11-29 01:57:57 -0500

    Saving Progress

--- sudokuruweb/src/app/App.tsx
+++ sudokuruweb/src/app/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.ts</code> and save to reload.
         </p>
         <p>
         {`This is ${process.env.REACT_APP_NAME}`}
