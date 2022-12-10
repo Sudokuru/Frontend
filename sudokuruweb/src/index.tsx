9a04bb427f3857c04db859a957bea229f206cdad
Galla <gallant.thomas@outlook.com>
2022-12-10 03:22:46 -0500

    Log-in and Log-out are working for website. Set up .env file so that it will be used locally and not stored in code. Created envTemplate.yml file so that structure of .env file is known.

--- sudokuruweb/src/index.tsx
+++ sudokuruweb/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -9,7 +10,13 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+          domain={process.env.REACT_APP_DOMAIN as string}
+          clientId={process.env.REACT_APP_CLIENT_ID as string}
+          redirectUri={window.location.origin}
+      >
+          <App />
+      </Auth0Provider>,
   </React.StrictMode>
 );
 
