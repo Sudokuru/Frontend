25c6243df48fd981a93824e471b54613bc67fd12
Galla <gallant.thomas@outlook.com>
2023-01-24 20:53:31 -0500

    Adding testIDs for all of the switches.

--- sudokuru/app/Components/Profile/SaveGamePreferencesToggle.tsx
+++ sudokuru/app/Components/Profile/SaveGamePreferencesToggle.tsx
@@ -60,42 +60,49 @@ const SaveGamePreferencesToggle = () => {
                 color={'red'}
                 value={isSaveGameHistoryEnabled}
                 onValueChange={toggleSaveGameHistorySwitch}
+                testID={isSaveGameHistoryEnabled ? "SaveGameHistoryEnabled" : "SaveGameHistoryDisabled"}
             />
             <Text>NotifyOnWrongCell</Text>
             <Switch
                 color={'red'}
                 value={isNotifyOnWrongCellEnabled}
                 onValueChange={toggleNotifyOnWrongCellSwitch}
+                testID={isNotifyOnWrongCellEnabled ? "NotifyOnWrongCellEnabled" : "NotifyOnWrongCellDisabled"}
             />
             <Text>HighlightAllSelectedNumber</Text>
             <Switch
                 color={'red'}
                 value={isHighlightAllSelectedNumberEnabled}
                 onValueChange={toggleHighlightAllSelectedNumberSwitch}
+                testID={isHighlightAllSelectedNumberEnabled ? "HighlightAllSelectedNumberEnabled" : "HighlightAllSelectedNumberDisabled"}
             />
             <Text>HighlightSelectedBox</Text>
             <Switch
                 color={'red'}
                 value={isHighlightSelectedBoxEnabled}
                 onValueChange={toggleHighlightSelectedBoxSwitch}
+                testID={isHighlightSelectedBoxEnabled ? "HighlightSelectedBoxEnabled" : "HighlightSelectedBoxDisabled"}
             />
             <Text>HighlightSelectedRow</Text>
             <Switch
                 color={'red'}
                 value={isHighlightSelectedRowEnabled}
                 onValueChange={toggleHighlightSelectedRowSwitch}
+                testID={isHighlightSelectedRowEnabled ? "HighlightSelectedRowEnabled" : "HighlightSelectedRowDisabled"}
             />
             <Text>PlayMusic</Text>
             <Switch
                 color={'red'}
                 value={isPlayMusicEnabled}
                 onValueChange={togglePlayMusicSwitch}
+                testID={isPlayMusicEnabled ? "PlayMusicEnabled" : "PlayMusicDisabled"}
             />
             <Text>MusicIntensify</Text>
             <Switch
                 color={'red'}
                 value={isMusicIntensifyEnabled}
                 onValueChange={toggleMusicIntensifySwitch}
+                testID={isMusicIntensifyEnabled ? "MusicIntensifyEnabled" : "MusicIntensifyDisabled"}
             />
         </View>
     );
--- sudokuru/app/Components/Profile/ThemeToggle.tsx
+++ sudokuru/app/Components/Profile/ThemeToggle.tsx
@@ -14,6 +14,7 @@ const ThemeToggle = () => {
                 color={'red'}
                 value={isThemeDark}
                 onValueChange={toggleTheme}
+                testID={isThemeDark ? "DarkThemeEnabled" : "DarkThemeDisabled"}
             />
         </View>
     );
