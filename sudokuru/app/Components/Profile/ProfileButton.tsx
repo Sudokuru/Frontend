1ca51d6f86266d8adafef8f9485283efb022b13b
DarthofLuke <60023923+DarthofLuke@users.noreply.github.com>
2023-01-31 15:25:31 -0500

    Creating deploy pipeline (#39)
    * Adding dist file to gitIgnore
    
    * moving createStackNavigator out of import list because it is a const declaration.
    
    * resolving typescript errors.
    
    * updating ts config and adding build command.
    
    * Adding build commands and deploy pipeline
    
    * updating readme and deploy command
    
    * removing unneeded command.
    
    * Added centralized settings.
    
    * Updating readme

--- sudokuru/app/Components/Profile/ProfileButton.tsx
+++ sudokuru/app/Components/Profile/ProfileButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const ProfileButton = () => {
 
-    const navigation = useNavigation();
+    const navigation: any = useNavigation();
 
     return (
         <Button mode="contained" testID={"ViewProfilePageButton"} onPress={() => navigation.navigate('Profile')}>
