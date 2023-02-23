5101644429e8ca5a151406c8c119c341d2eee1bc
Galla <gallant.thomas@outlook.com>
2023-02-22 23:23:59 -0500

    cleaning up code

--- sudokuru/app/Components/Auth0/LoginButton.tsx
+++ sudokuru/app/Components/Auth0/LoginButton.tsx
@@ -8,8 +8,7 @@ import {Button} from "react-native-paper"
 import { DOMAIN, CLIENT_ID } from "../../../config"
 import { Auth0JwtPayload } from "../../../app.config"
 import Constants, {AppOwnership} from "expo-constants";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import {getTokenExp, getTokenName, removeValue, storeData} from "../../Functions/Auth0/token";
+import {getTokenName, removeValue, storeData} from "../../Functions/Auth0/token";
 
 // You need to swap out the Auth0 client id and domain with the one from your Auth0 client.
 // In your Auth0 client, you need to also add a url to your authorized redirect urls.
