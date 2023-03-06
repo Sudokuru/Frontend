d96c72c108d61b40eb405e5f86b6d700b6697685
Galla <gallant.thomas@outlook.com>
2023-03-05 20:23:03 -0500

    Adding startLesson button

--- sudokuru/app/Components/Home/Carousel.tsx
+++ sudokuru/app/Components/Home/Carousel.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
-import { Button, View, StyleSheet, Dimensions, Pressable, Text, Image, LogBox, TouchableNativeFeedback, Platform, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+    Pressable,
+    Text,
+    Image,
+    LogBox,
+    TouchableNativeFeedback,
+    Platform,
+    TouchableOpacity,
+    useWindowDimensions
+} from 'react-native';
+import { Button } from 'react-native-paper';
 import Carousel from 'react-native-reanimated-carousel';
+import {useNavigation} from "@react-navigation/native";
 
 const Ccarousel = () =>{
+
+        //const size = useWindowDimensions();
+        //const width = Math.min(size.width, size.height);
+        //console.log(width);
     const width = Dimensions.get('window').width;
+
+    const navigation: any = useNavigation();
+
+    const [index, setIndex] = useState(0);
+
+    function navigate(index: number):any {
+        if (index == 0){
+            navigation.navigate('Landing');
+        }
+        if (index == 1){
+            navigation.navigate('Drill')
+        }
+        if (index == 2){
+            navigation.navigate('Learn')
+        }
+        if (index == 3){
+            navigation.navigate('Landing');
+        }
+        if (index == 4){
+            navigation.navigate('Landing');
+        }
+        if (index == 5){
+            navigation.navigate('Landing');
+        }
+    };
 
     return(
         <View>
@@ -19,24 +62,26 @@ const Ccarousel = () =>{
                   parallaxScrollingOffset: 1250,
                 }}
                 scrollAnimationDuration={100}
-                onSnapToItem={(index) => console.log('current index:', index)}
+                onSnapToItem={(index) => setIndex(index)}
                 renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                            backgroundColor: '#F2F2F2'
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-
-                    </View>
+                        <View
+                            style={{
+                                flex: 1,
+                                borderWidth: 1,
+                                justifyContent: 'center',
+                                backgroundColor: '#F2F2F2'
+                            }}
+                        >
+                            <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                                {index}
+                            </Text>
+                        </View>
                 )}
             />
+            <Button onPress={() => navigate(index)}>
+                Start Lesson
+            </Button>
         </View>    );
-}
+};
 
 export default Ccarousel;
--- sudokuru/app/Pages/HomePage.tsx
+++ sudokuru/app/Pages/HomePage.tsx
@@ -68,7 +68,6 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
