1ac73885f535f7c388df4f89b2ffac076ccdfc58
danielcosentino <daniel.cosentino@knights.ucf.edu>
2023-03-06 19:35:29 -0500

    added comments

--- sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
+++ sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
@@ -269,11 +269,11 @@ const ActionRow = (props) => {
             {/* Note mode */}
             <TouchableOpacity style={styles(cellSize).actionControlButton} onPress={toggleNoteMode}>
                 {inNoteMode 
-                    ? 
+                    ? // note mode on
                     <Svg height="100%" width="100%" viewBox="0 0 50 50">
                         <Path d="M9 39h2.2l22.15-22.15-2.2-2.2L9 36.8Zm30.7-24.3-6.4-6.4 2.1-2.1q.85-.85 2.1-.85t2.1.85l2.2 2.2q.85.85.85 2.1t-.85 2.1Zm-2.1 2.1L12.4 42H6v-6.4l25.2-25.2Zm-5.35-1.05-1.1-1.1 2.2 2.2Z"/>
                     </Svg>
-                    : 
+                    : // note mode off
                     <Svg height="100%" width="100%" viewBox="0 0 50 50">
                         <Path d="M39.8 44.2 25 29.4 12.4 42H6v-6.4L18.6 23 3.8 8.2l2.1-2.1 36 36Zm-18-18-1.1-1.1 1.1 1.1 1.1 1.1Zm7.4-1-2.1-2.1 6.25-6.25-2.2-2.2-6.25 6.25-2.1-2.1 8.4-8.4 6.4 6.4Zm10.5-10.5-6.4-6.4 2.1-2.1q.85-.85 2.125-.825 1.275.025 2.125.875L41.8 8.4q.85.85.875 2.075Q42.7 11.7 41.8 12.6ZM26 22ZM9 39h2.2l11.7-11.7-2.2-2.2L9 36.8Z" />
                     </Svg>
