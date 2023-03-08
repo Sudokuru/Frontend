c64f5789538ebc8fa5273db8ae02585e83e15868
Thomas Gallant <60023923+Thomas-Gallant@users.noreply.github.com>
2023-03-07 21:36:43 -0500

    Adding carousel buttons and making Lesson Icons clickable (#62)
    * carousel works plz
    
    * made container1
    
    * Adding startLesson button
    
    * Added import
    
    * Adding navigation to each of the lessons.
    
    * Adding name to lesson sliders
    Adding puzzle back
    
    * Refactoring Home page to be named Home page and not profile page
    
    * Cleaning up menu
    
    * Fixing issue
    
    * Adding slider
    
    * made container1
    
    * reinstalled slider
    
    * Removing menu sidebar
    
    * added start button to home page on web
    
    * Removing unused imports
    
    * UpdatingBoardBackground
    
    * adding patchPackage dependency
    
    * Cleaning up formatting
    
    * Fixing scroll issue with Web
    
    * Adding design decisions documentation
    
    * Adding Header component to home page
    
    * Making progress on landing page resizing
    
    * Buttons are working!
    
    * Buttons are working!
    
    * Finalizing Changes
    
    * Improving web device-compatibility for button sizing.
    
    * Switching from "Forward" and "Back" to arrow buttons
    
    ---------
    
    Co-authored-by: Mink234 <mkim5225@knights.ucf.edu>

--- sudokuru/app/Components/Home/Carousel.tsx
+++ sudokuru/app/Components/Home/Carousel.tsx
@@ -1,32 +1,23 @@
 import React, {useState} from 'react';
 import {
     View,
-    StyleSheet,
-    Dimensions,
-    Pressable,
     Text,
-    Image,
-    LogBox,
-    TouchableNativeFeedback,
     Platform,
-    TouchableOpacity,
     useWindowDimensions
 } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
+import Carousel, {ICarouselInstance} from 'react-native-reanimated-carousel';
 import { Button } from 'react-native-paper';
 import {useNavigation} from "@react-navigation/native";
+import {AntDesign} from "@expo/vector-icons";
 
 const Ccarousel = () =>{
 
-        //const size = useWindowDimensions();
-        //const width = Math.min(size.width, size.height);
-        //console.log(width);
-    //const width = Dimensions.get('window').width;
     const size = useWindowDimensions();
     const width = Math.min(size.width, size.height);
 
     const navigation: any = useNavigation();
 
+    const ref = React.useRef<ICarouselInstance>(null);
     const [index, setIndex] = useState(0);
 
     function navigate(index: number):any {
@@ -62,78 +53,101 @@ const Ccarousel = () =>{
     }
 
     if(Platform.OS === 'web'){
-    return(
-        <View>
-            <Carousel
-                loop
-                width={width}
-                height={width / 4}
-                autoPlay={false}
-                data={[...new Array(12).keys()]}
-                mode='parallax'
-                modeConfig={{
-                  parallaxScrollingScale: 0.5,
-                  parallaxScrollingOffset: 1250,
-                }}
-                scrollAnimationDuration={100}
-                onSnapToItem={(index) => setIndex(index)}
-                renderItem={({ index }) => (
-                        <View
-                            style={{
-                                flex: 1,
-                                borderWidth: 1,
-                                justifyContent: 'center',
-                                backgroundColor: '#F2F2F2'
-                            }}
-                        >
-                            <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                                {getLessonName(index)}
-                            </Text>
-                        </View>
-                )}
-            />
-            <Button onPress={() => navigate(index)}>
-                Start Lesson
-            </Button>
-        </View>    );
-        }
- else{
-    return(
-    <View>
-    <Carousel
+        return(
+            <View>
+                <Carousel
                     loop
                     width={width}
-                    height={width / 1.5}
+                    height={width / 4}
+                    ref={ref}
                     autoPlay={false}
-                    data={[...new Array(6).keys()]}
+                    data={[...new Array(12).keys()]}
                     mode='parallax'
                     modeConfig={{
-                      parallaxScrollingScale: 0.8,
-                      parallaxScrollingOffset: 120,
+                      parallaxScrollingScale: 0.5,
+                      parallaxScrollingOffset: 1250,
                     }}
-                    scrollAnimationDuration={200}
+                    scrollAnimationDuration={100}
                     onSnapToItem={(index) => setIndex(index)}
                     renderItem={({ index }) => (
-                        <View
-                            style={{
-                                flex: 1,
-                                borderWidth: 1,
-                                justifyContent: 'center',width:360, right: -25,
-                                backgroundColor: '#F2F2F2'
-                            }}
-                        >
-                            <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                                {getLessonName(index)}
-                            </Text>
-                        </View>
+                            <View
+                                style={{
+                                    flex: 1,
+                                    borderWidth: 1,
+                                    justifyContent: 'center',
+                                    backgroundColor: '#F2F2F2'
+                                }}
+                            >
+                                {/*When user touches Lesson Icon text they will be navigated to the given lesson*/}
+                                <Button style={{alignItems: 'center'}} onPress={() => navigate(index)}>
+                                    <Text style={{ fontSize: 30 }}>{getLessonName(index)}</Text>
+                                </Button>
+                            </View>
                     )}
                 />
-            <Button onPress={() => navigate(index)}>
-                Start Lesson
-            </Button>
-        </View> );
-
- }
+                <View style={{ justifyContent: 'center', flexDirection: 'row'}}>
+                    <Button onPress={() => ref.current?.scrollTo({count: -1})}>
+                        <AntDesign name="leftcircleo" size={18}/>
+                    </Button>
+                    <Button onPress={() => navigate(index)}>
+                        Start Lesson
+                    </Button>
+                    <Button onPress={() => ref.current?.scrollTo({count: 1})}>
+                        <AntDesign name="rightcircleo" size={18}/>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+     else{
+        return(
+            <View>
+                <Carousel
+                        loop
+                        width={width}
+                        height={width / 1.5}
+                        ref={ref}
+                        autoPlay={false}
+                        data={[...new Array(6).keys()]}
+                        mode='parallax'
+                        modeConfig={{
+                          parallaxScrollingScale: 0.8,
+                          parallaxScrollingOffset: 120,
+                        }}
+                        scrollAnimationDuration={200}
+                        onSnapToItem={(index) => setIndex(index)}
+                        renderItem={({ index }) => (
+                            <View
+                                style={{
+                                    flex: 1,
+                                    borderWidth: 1,
+                                    justifyContent: 'center',
+                                    backgroundColor: '#F2F2F2'
+                                }}
+                            >
+                                {/*When user touches Lesson Icon they will be navigated to the given lesson*/}
+                                <Button contentStyle={{height: '100%'}} onPress={() => navigate(index)}>
+                                    {getLessonName(index)}
+                                </Button>
+                            </View>
+                        )}
+                    />
+                    <View style={{ justifyContent: 'center', flexDirection: 'row'}}>
+                        <Button onPress={() => ref.current?.scrollTo({count: -1})}>
+                            <AntDesign name="leftcircleo" size={18}/>
+                        </Button>
+                        <Button onPress={() => navigate(index)}>
+                            Start Lesson
+                        </Button>
+                        <Button onPress={() => ref.current?.scrollTo({count: 1})}>
+                            <AntDesign name="rightcircleo" size={18}/>
+                        </Button>
+                    </View>
+            </View>
+        );
+     }
 };
+
+
 
 export default Ccarousel;
