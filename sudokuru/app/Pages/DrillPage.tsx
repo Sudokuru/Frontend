8b9c6699a1221adb067ab90d2bc63395340f354e
Cosentino <79738516+danielcosentino@users.noreply.github.com>
2023-03-17 14:46:41 -0400

    Hint API call is functional (#83)
    * hint button gets the response from the api
    
    * removed print statements
    
    * drill page takes in strat as a parameter

--- sudokuru/app/Pages/DrillPage.tsx
+++ sudokuru/app/Pages/DrillPage.tsx
@@ -10,7 +10,7 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useFonts, Inter_100Thin, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter';
 import { makePuzzle, pluck, makeBoard } from '../Components/Sudoku Board/sudoku';
-import { List } from 'immutable';
+import { Set, List } from 'immutable';
 import Header from "../Components/Header";
 
 const sudokuru = require("../../node_modules/sudokuru/dist/bundle.js"); // -- What works for me
@@ -19,7 +19,7 @@ const sudokuru = require("../../node_modules/sudokuru/dist/bundle.js"); // -- Wh
 const Puzzles = sudokuru.Puzzles;
 const Drills = sudokuru.Drills;
 
-function formatPuzzle(str) {
+function strPuzzleToArray(str) {
   let arr = [];
   for (let i = 0; i < str.length; i += 9) {
     arr.push(str.slice(i, i + 9).split('').map(Number));
@@ -86,46 +86,96 @@ function parseApiAndAddNotes(board, puzzleCurrentNotesState)
   return board;
 }
 
+function componentBoardValsToArray(board)
+{
+  let boardArray = [];
+  let temp = [];
+  for (let i = 0; i < 9; i++)
+  {
+    temp = [];
+    for (let j = 0; j < 9; j++)
+    {
+      currVal = board.get('puzzle').getIn([i, j, 'value']);
+      temp.push(!currVal ? "0" : currVal.toString());
+    }
+    boardArray.push(temp);
+  }
+  return boardArray;
+}
 
-const DrillPage = () => {
-    let [fontsLoaded] = useFonts({
-        Inter_100Thin, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_700Bold
+function componentBoardNotesToArray(board)
+{
+  let notesArray = [];
+  let temp = [];
+  for (let i = 0; i < 9; i++)
+  {
+    for (let j = 0; j < 9; j++)
+    {
+      temp = [];
+      let notesSetFromComponent = board.get('puzzle').getIn([i, j, 'notes']);
+      if (!notesSetFromComponent) 
+      {
+        notesArray.push(temp);
+        continue;
+      }
+      for (let k = 1; k <= 9; k++)
+      {
+        if (notesSetFromComponent.includes(k))
+        {
+          temp.push((k).toString());
+        }
+      }
+      notesArray.push(temp);
+    }
+  }
+  return notesArray;
+}
+
+const DrillPage = (props) => {
+  let strategy = props.route.params ? props.route.params.params : "no props.route.params in DrillPage"
+  console.log(strategy);
+  let [fontsLoaded] = useFonts({
+      Inter_100Thin, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  async function generateGame(url, strategies, token) {
+    let board = await Drills.getGame(url, strategies, token).then(game =>
+    {
+      let board = makeBoard(strPuzzleToArray(game.puzzleCurrentState))
+      board = parseApiAndAddNotes(board, game.puzzleCurrentNotesState);
+      return board;
     });
 
-    if (!fontsLoaded) {
-        return null;
-    }
+    return {
+      board, history: List.of(board), historyOffSet: 0,
+    };
+  }
 
-    // TODO: This should eventually call greg's API for making the puzzle
-    async function generateGame (url, strategies, token) {
-      let board = await Drills.getGame(url, strategies, token).then(game =>
-      {
-        console.log(game);
-        let board = makeBoard(formatPuzzle(game.puzzleCurrentState))
-        board = parseApiAndAddNotes(board, game.puzzleCurrentNotesState);
-        return board;
-      });
-      console.log("Board: ", board);
+  function getHint(board) {
+    let boardArray = componentBoardValsToArray(board);
+    let notesArray = componentBoardNotesToArray(board);
+    let hint = Puzzles.getHint(boardArray, notesArray, strategy)
+    console.log(hint);
+  }
 
-      return {
-        board, history: List.of(board), historyOffSet: 0,
-      };
-    }
-
-    return (
-        <SafeAreaProvider>
-            <SafeAreaView>
-                <Header page={'Sudoku'}/>
-                <View style={homeScreenStyles.home}>
-                    <View style={styles.container}>
-                        {/* The game now required the info about it to be rendered, which is given in generateGame() */}
-                        <SudokuBoard generatedGame={generateGame("http://localhost:3001/",  ["NAKED_SINGLE"], "token")} isDrill={true}/>
-                        <StatusBar style="auto" />
-                    </View>
-                </View>
-            </SafeAreaView>
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Header page={'Sudoku'}/>
+        <View style={homeScreenStyles.home}>
+          <View style={styles.container}>
+            {/* The game now required the info about it to be rendered, which is given in generateGame() */}
+            <SudokuBoard generatedGame={generateGame("http://localhost:3001/",  ["NAKED_SINGLE"], "token")} isDrill={true} getHint={getHint}/>
+            <StatusBar style="auto" />
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 };
 
 const styles = StyleSheet.create({
