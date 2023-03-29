dbb98ad3aa1452d97d9ff1fdde682ae0acd067b4
Galla <gallant.thomas@outlook.com>
2023-03-29 01:38:00 -0400

    Cleaning up Lessons page and making it dynamically fit

--- sudokuru/app/Pages/Lesson.tsx
+++ sudokuru/app/Pages/Lesson.tsx
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
-import {StyleSheet, View, Image, Platform} from "react-native";
+import {StyleSheet, View, Image, Platform, useWindowDimensions, Pressable} from "react-native";
 
 import Header from "../Components/Header";
-import { Text, Button } from 'react-native-paper';
+import {Text, Button, useTheme} from 'react-native-paper';
 import {useNavigation} from "@react-navigation/native";
-import {AntDesign} from "@expo/vector-icons";
+import {AntDesign, MaterialCommunityIcons} from "@expo/vector-icons";
+import Alert from "react-native-awesome-alerts";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
 
 const sudokuru = require("../../node_modules/sudokuru/dist/bundle.js"); // -- What works for me
 const Lessons = sudokuru.Lessons;
 
 const Lesson = (props: { route: { params: { params: any; }; }; }) => {
-      //Brings in name of strategy from carousel
-      let name = props.route.params ? props.route.params.params : "no props.route.params in LessonPage"
-      const navigation: any = useNavigation();
+    //Brings in name of strategy from carousel
+    let name = props.route.params ? props.route.params.params : "no props.route.params in LessonPage"
+    const navigation: any = useNavigation();
 
-      function getTitle(name: string):string {
-            let lessonName: string;
-            name == "AMEND_NOTES" ? lessonName = 'Amend Notes' :
-                name == "NAKED_SINGLE" ? lessonName = 'Naked Single' :
-                    name == "NAKED_SET" ? lessonName = 'Naked Set' :
-                        name == "HIDDEN_SINGLE" ? lessonName = 'Hidden Single' :
-                            name == "HIDDEN_SET" ? lessonName = 'Hidden Set' : lessonName = 'Null';
-            return lessonName;
-      }
+    const size = useWindowDimensions();
+    const reSize = Math.min(size.width, size.height);
+
+    const [learnHelpVisible, setLearnHelpVisible] = React.useState(false);
+    const showLearnHelp = () => setLearnHelpVisible(true);
+    const hideLearnHelp = () => setLearnHelpVisible(false);
+
+    const theme = useTheme();
+
+    function getTitle(name: string):string {
+        let lessonName: string;
+        name == "AMEND_NOTES" ? lessonName = 'Amend Notes' :
+            name == "NAKED_SINGLE" ? lessonName = 'Naked Single' :
+                name == "NAKED_SET" ? lessonName = 'Naked Set' :
+                    name == "HIDDEN_SINGLE" ? lessonName = 'Hidden Single' :
+                        name == "HIDDEN_SET" ? lessonName = 'Hidden Set' : lessonName = 'Null';
+        return lessonName;
+    }
 
       let title = getTitle(name);
 
@@ -55,125 +66,78 @@ const Lesson = (props: { route: { params: { params: any; }; }; }) => {
          console.log("FINISHED LESSON")
       }
 
-      if(Platform.OS === 'web'){
+      console.log(reSize / 25, "THIS IS SIZE", theme.colors.primary, title);
+
       return (
-          <View>
-              <Header page={'Lesson'}/>
-              <View>
-                   <View style={styles.container1}>
+          <SafeAreaProvider>
+              <SafeAreaView style={{height: '100%', width: '100%'}}>
+                  <Header page={'Lesson'}/>
+                  <View>
+                      <View style={styles.container1}>
 
-                      <Text>
-                          <Text>{title}</Text>
-                          <Text>{" Lesson"}</Text>
-                      </Text>
+                          <View style={{flexDirection: 'row'}}>
+                              <Text style={{color: theme.colors.primary, fontSize: reSize/25,  fontWeight: 'bold'}}>{title + " Lesson"}</Text>
+                              <Pressable onPress={() => showLearnHelp()}>
+                                  <MaterialCommunityIcons color={theme.colors.onPrimary} name="help"/>
+                              </Pressable>
+                          </View>
 
-                      <Text>{" "}</Text>
-                      <View style={{ justifyContent: 'center', flexDirection: 'row'}}>
+                          <View style={{ justifyContent: 'center', flexDirection: 'row'}}>
 
-                          <Button style={{right:25, height: 40, top:200}} mode="contained" disabled={count - 1 == -1} onPress={() => setCount(count - 1)}>
-                            <AntDesign name="leftcircleo" size={18}/>
-                          </Button>
+                              <Pressable style={{top: reSize/4.5, height: reSize/10, right: reSize/25}} disabled={count - 1 == -1} onPress={() => setCount(count - 1)}>
+                                  <AntDesign color="white" name="leftcircleo" size={reSize/15}/>
+                              </Pressable>
 
-                          {
-                              (teach[count][1] != null) ?
-                              <Image
-                              style={{width: 500, height: 512}}
-                              source={{uri:teach[count][1]}}
-                              /> : <></>
-                          }
+                              {
+                                  (teach[count][1] != null) ?
+                                      <Image
+                                          style={{width: reSize/2, height: reSize/2}}
+                                          source={{uri:teach[count][1]}}
+                                      /> : <></>
+                              }
 
-                          <Button style={{left:25, height: 40, top:200}} mode="contained" disabled={count + 1 == teach.length} onPress={() =>setCount(count + 1)}>
-                            <AntDesign name="rightcircleo" size={18}/>
-                          </Button>
+                              <Pressable style={{top: reSize/4.5, height: reSize/10, left: reSize/25}} onPress={() => setCount(count + 1)} disabled={count + 1 == teach.length}>
+                                  <AntDesign color="white" name="rightcircleo" size={reSize/15}/>
+                              </Pressable>
 
+                          </View>
+                          <Text>{" "}</Text>
+                          <View style={{width: reSize/1.5}}>
+                              {
+                                  (teach[count][0] != null) ?
+                                      <Text style={{color: theme.colors.onPrimary, textAlign: 'justify', fontSize: reSize/50}}>{teach[count][0]}</Text>
+                                      : <></>
+                              }
+                          </View>
                       </View>
-                      <Text>{" "}</Text>
-                       {
-                           (teach[count][0] != null) ?
-                           <Text>{teach[count][0]}</Text>
-                           : <></>
-                       }
                   </View>
-              </View>
-          </View>
-      );}
-      else{
-      return (
-          <View>
-              <Header page={'Lesson'}/>
-              <View>
-                   <View style={styles.container2}>
-
-
-                      <Text>{" "}</Text>
-                      <Text>
-                          <Text>{title}</Text>
-                          <Text>{" Lesson"}</Text>
-                      </Text>
-
-                      <Text>{" "}</Text>
-
-                       {
-                           (teach[count][1] != null) ?
-                           <Image
-                               style={{width: 350, height: 448}}
-                               source={{uri:teach[count][1]}}
-                           /> : <></>
-                       }
-                      <Text>{" "}</Text>
-
-                       {
-                           (teach[count][0] != null) ?
-                           <Text>{teach[count][0]}</Text>
-                           : <></>
-                       }
-
-                      <View style={{ justifyContent: 'center', flexDirection: 'row', top:50}}>
-
-                          <Button style={{right:80}} mode="contained" disabled={count - 1 == -1} onPress={() => setCount(count - 1)}>
-                            <AntDesign name="leftcircleo" size={20}/>
-                          </Button>
-                          <Button style={{left:80}} mode="contained" disabled={count + 1 == teach.length} onPress={() =>setCount(count + 1)}>
-                            <AntDesign name="rightcircleo" size={20}/>
-                          </Button>
-
-                      </View>
-
-                  </View>
-              </View>
-          </View>
-      );}
-
+                  <Alert
+                      show={learnHelpVisible}
+                      title="Learning Help"
+                      message=
+                      {`This is the `+ title + ' lesson page.\n\n' +
+                       'Navigate through the lesson by using the left and right arrows.\n\n' +
+                       'Each page of the lesson will contain a board as well as a description to help you understand the process for using the ' + title + ' strategy'
+                      }
+                      messageStyle={{maxWidth: 500}}
+                      showConfirmButton={true}
+                      closeOnTouchOutside={false}
+                      closeOnHardwareBackPress={false}
+                      confirmText={"OK"}
+                      confirmButtonColor={theme.colors.background}
+                      onConfirmPressed={() => {
+                          hideLearnHelp();
+                      }}
+                  />
+              </SafeAreaView>
+          </SafeAreaProvider>
+      );
 };
 
 const styles = StyleSheet.create({
       container1: {
-          flex: 1,
           alignItems: 'center',
           justifyContent: 'center'
-      },
-      container2: {
-          alignItems: 'center',
-          justifyContent: 'center',
-      },
-});
-
-const homeScreenStyles = StyleSheet.create({
-      home: {
-          display: "flex",
-          flexDirection: 'row',
-          //backgroundColor: 'red',
-      },
-      homeMenu: {
-          //backgroundColor: 'red',
-          width: "15%",
-      },
-      lessons: {
-          //backgroundColor: 'blue',
-          width: "85%",
-          alignContent: "flex-start",
-          flexDirection: 'row',
-          flexWrap: "wrap",
       },
 });
 
