27ece1370d7b7937e48ec9554b87daab73b32b33
danielcosentino <daniel.cosentino@knights.ucf.edu>
2023-04-03 17:49:24 -0400

    moved handleKeyDown to the top level view

--- sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
+++ sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
@@ -230,7 +230,7 @@ const NumberControl = (props) => {
             inNoteMode
               ? addNumberAsNote(number)
               : fillNumber(number);
-          }
+        }
         return (
           <Pressable key={number} onPress={onClick} disabled={prefilled || inHintMode} style={ styles(cellSize).numberContainer }>
             <Text style={styles(cellSize).numberControlText}>{number}</Text>
@@ -466,7 +466,7 @@ let puzzleString = "";
 let notesString = "";
 
 const Cell = (props) => {
-  const { value, onClick, onValueChange, isPeer, isSelected, sameValue, prefilled, notes, conflict, x, y, eraseSelected, inHintMode, hintSteps, currentStep, game, navigation } = props;
+  const { value, onClick, isPeer, isSelected, sameValue, prefilled, notes, conflict, x, y, inHintMode, hintSteps, currentStep, game, navigation } = props;
   const cellSize = getCellSize();
 
   let bgColor = '#808080';
@@ -596,15 +596,6 @@ const Cell = (props) => {
     }
   }
 
-  const handleKeyDown = (event) => {
-    const inputValue = event.nativeEvent.key;
-    if (/^[1-9]$/.test(inputValue) && !inHintMode) { // check if input is a digit from 1 to 9
-      onValueChange(x, y, parseInt(inputValue, 10));
-    }
-    if ((inputValue == "Delete" || inputValue == "Backspace") && !inHintMode)
-      eraseSelected();
-  };
-
   const getNoteContents = (noteVal) =>
   {
     if (notes.has(noteVal))
@@ -618,7 +609,7 @@ const Cell = (props) => {
   }
 
   return (
-    <Pressable onPress={() => onClick(x, y)} onKeyDown={handleKeyDown}>
+    <Pressable onPress={() => onClick(x, y)}>
       <View style={[styles(cellSize).cellView,
         (x % 3 === 0) && {borderLeftWidth: styles(cellSize).hardLineThickness.thickness},
         (y % 3 === 0) && {borderTopWidth: styles(cellSize).hardLineThickness.thickness},
@@ -668,7 +659,6 @@ const Cell = (props) => {
 Cell.propTypes = {
     value: PropTypes.number,
     onClick: PropTypes.func.isRequired,
-    onValueChange: PropTypes.func.isRequired,
     isPeer: PropTypes.bool.isRequired,
     isSelected: PropTypes.bool.isRequired,
     sameValue: PropTypes.bool.isRequired,
@@ -1284,19 +1274,10 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
     let hintSteps = board.get('hintSteps');
     let currentStep = board.get('currentStep');
 
-    const handleValueChange = (x, y, newValue) => {
-      let { board } = this.state;
-      let inNoteMode = board.get('inNoteMode');
-
-      if (inNoteMode) this.addNumberAsNote(newValue);
-      else this.fillNumber(newValue);
-    };
-
     const { navigation } = this.props;
 
     let game = null;
     if (!this.props.isDrill) game = this.state.activeGame[0];
-
 
         return (
             <Cell
@@ -1307,7 +1288,6 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
                 isPeer={peer}
                 value={value}
                 onClick={(x, y) => { this.selectCell(x, y); }}
-                onValueChange={handleValueChange}
                 key={y}
                 x={x}
                 y={y}
@@ -1396,6 +1376,19 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
   checkMarkClicked = () => {
     this.toggleHintMode()
   }
+
+  handleKeyDown = (event) => {
+    const { board } = this.state;
+    let inHintMode = board.get('inHintMode');
+    let inNoteMode = board.get('inNoteMode');
+    const inputValue = event.nativeEvent.key;
+    if (/^[1-9]$/.test(inputValue) && !inHintMode) { // check if input is a digit from 1 to 9
+      if (inNoteMode) this.addNumberAsNote(parseInt(inputValue, 10));
+      else this.fillNumber(parseInt(inputValue, 10));
+    }
+    if ((inputValue == "Delete" || inputValue == "Backspace") && !inHintMode)
+      this.eraseSelected();
+  };
 
   renderPuzzle = () => {
     const { board } = this.state;
@@ -1525,12 +1518,11 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
     {
       this.props.generatedGame.then(game => this.setState(game));
     }
-
     
     drillMode = this.props.isDrill;
 
     return (
-      <View>
+      <View onKeyDown={this.handleKeyDown}>
         {board && !this.props.isDrill && this.renderTopBar()}
         {board && this.renderPuzzle()}
         {board &&
