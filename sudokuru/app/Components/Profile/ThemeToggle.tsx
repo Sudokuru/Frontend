302cc3d01b9a0efac410600e8d77f543215ca94e
Galla <gallant.thomas@outlook.com>
2023-04-10 11:43:25 -0400

    Removing unneeded code

--- sudokuru/app/Components/Profile/ThemeToggle.tsx
+++ sudokuru/app/Components/Profile/ThemeToggle.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import { useTheme, Switch, Text } from 'react-native-paper';
+import { Switch, Text } from 'react-native-paper';
 import { PreferencesContext } from '../../Contexts/PreferencesContext';
 
 const ThemeToggle = () => {
-    const theme = useTheme();
     const { toggleTheme, isThemeDark } = React.useContext(PreferencesContext);
-
-    console.log(isThemeDark, "HAHA");
 
     return (
         <View>
