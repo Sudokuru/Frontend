83d03548c075a6304a6a22edf9a24309088e1f36
Arthur Upfield <arthurupfield@outlook.com>
2023-04-11 19:34:55 -0400

    added strategies so that the board is solvable

--- sudokuru/app/Pages/LandingPage.tsx
+++ sudokuru/app/Pages/LandingPage.tsx
@@ -34,8 +34,8 @@ const LandingPage = () => {
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
     const newUser = 1;
-
-    let strategies = ["AMEND_NOTES", "SIMPLIFY_NOTES", "NAKED_SINGLE", "HIDDEN_SINGLE", "NAKED_SET", "HIDDEN_SET"];
+    
+    let strategies = ["AMEND_NOTES", "SIMPLIFY_NOTES", "NAKED_SINGLE", "HIDDEN_SINGLE", "NAKED_SET", "HIDDEN_SET", "NAKED_PAIR", "NAKED_TRIPLET", "NAKED_QUADRUPLET", "HIDDEN_PAIR", "HIDDEN_TRIPLET", "HIDDEN_QUADRUPLET"];
 
     let [fontsLoaded] = useFonts({
         Inter_100Thin, Inter_200ExtraLight, Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_700Bold
