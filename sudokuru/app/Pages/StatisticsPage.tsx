985124c55991a0d9ed4db19271f5ad72b6bb0e7f
Galla <gallant.thomas@outlook.com>
2023-04-11 11:18:26 -0400

    Saving total statistics

--- sudokuru/app/Pages/StatisticsPage.tsx
+++ sudokuru/app/Pages/StatisticsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, View} from "react-native";
+import {ScrollView, StyleSheet, View} from "react-native";
 import {Button, useTheme, Text, ActivityIndicator} from 'react-native-paper';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Header from "../Components/Header"; 
@@ -10,6 +10,8 @@ import {useNavigation} from "@react-navigation/native";
 import {VictoryChart, VictoryLine, VictoryTheme} from "victory-native";
 import {PreferencesContext} from "../Contexts/PreferencesContext";
 import {useFocusEffect} from "@react-navigation/core";
+import TotalStatistics from "../Components/Statistics/TotalStatistics";
+import {retrieveTotalStatistics} from "../Functions/Statistics/StatisticsParsing";
 
 // Sudokuru Package Import
 const sudokuru = require("../../node_modules/sudokuru/dist/bundle.js");
@@ -27,6 +29,7 @@ const StatisticsPage = () => {
   const { updateLearnedLessons, learnedLessons } = React.useContext(PreferencesContext);
 
   const [isLoading, setLoading] = React.useState(true);
+  const [totalStatistics, setTotalStatistics] = React.useState<any>();
 
   async function deleteUserStatistics(url: string) {
 
@@ -57,6 +60,7 @@ const StatisticsPage = () => {
     await Statistics.getStatistics(url, token).then((res: any) => {
       if (res) {
         console.log(res);
+        setTotalStatistics(retrieveTotalStatistics(res));
         setLoading(false);
       }
       else {
@@ -86,212 +90,53 @@ const StatisticsPage = () => {
         <SafeAreaView style={{height: '100%', width: '100%'}}>
           <ScrollView>
             <Header page="Statistics" />
-            <Button mode="contained" onPress={() => {
-              deleteUserStatistics(USERGAMESTATISTICSBFFURL);
-            }}>
-              Delete Statistics
-            </Button>
-            <Button mode="contained" onPress={() => {
-              getUserStatistics(USERGAMESTATISTICSBFFURL);
-            }}>
-              Get User Statistics
-            </Button>
-            <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-              <VictoryLine
-                  style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                  }}
-                  data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 4 },
-                    { x: 6, y: 4 },
-                    { x: 7, y: 4 },
-                    { x: 8, y: 4 },
-                    { x: 9, y: 4 },
-                    { x: 10, y: 4 },
-                    { x: 11, y: 4 },
-                    { x: 12, y: 4 },
-                    { x: 13, y: 4 },
-                    { x: 14, y: 4 },
-                    { x: 15, y: 4 },
-                    { x: 16, y: 4 },
-                    { x: 17, y: 4 },
-                    { x: 18, y: 4 },
-                    { x: 19, y: 4 },
-                    { x: 20, y: 4 },
-                    { x: 21, y: 4 },
-                    { x: 22, y: 4 },
-                    { x: 23, y: 4 },
-                    { x: 24, y: 4 },
-                    { x: 25, y: 7 }
-                  ]}
+            <View style={styles.container}>
+              <TotalStatistics
+                  totalScore={totalStatistics.totalScore}
+                  numGamesPlayed={totalStatistics.numGamesPlayed}
+                  fastestSolveTime={totalStatistics.fastestSolveTime}
+                  averageSolveTime={totalStatistics.averageSolveTime}
+                  totalSolveTime={totalStatistics.totalSolveTime}
+                  numHintsUsed={totalStatistics.numHintsUsed}
+                  numWrongCellsPlayed={totalStatistics.numWrongCellsPlayed}
               />
-            </VictoryChart>
+              <Button mode="contained" onPress={() => {
+                deleteUserStatistics(USERGAMESTATISTICSBFFURL);
+              }}>
+                Delete Statistics
+              </Button>
+            </View>
 
-            <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-              <VictoryLine
-                  style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                  }}
-                  data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 4 },
-                    { x: 6, y: 4 },
-                    { x: 7, y: 4 },
-                    { x: 8, y: 4 },
-                    { x: 9, y: 4 },
-                    { x: 10, y: 4 },
-                    { x: 11, y: 4 },
-                    { x: 12, y: 4 },
-                    { x: 13, y: 4 },
-                    { x: 14, y: 4 },
-                    { x: 15, y: 4 },
-                    { x: 16, y: 4 },
-                    { x: 17, y: 4 },
-                    { x: 18, y: 4 },
-                    { x: 19, y: 4 },
-                    { x: 20, y: 4 },
-                    { x: 21, y: 4 },
-                    { x: 22, y: 4 },
-                    { x: 23, y: 4 },
-                    { x: 24, y: 4 },
-                    { x: 25, y: 7 }
-                  ]}
-              />
-            </VictoryChart>
-
-            <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-              <VictoryLine
-                  style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                  }}
-                  data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 4 },
-                    { x: 6, y: 4 },
-                    { x: 7, y: 4 },
-                    { x: 8, y: 4 },
-                    { x: 9, y: 4 },
-                    { x: 10, y: 4 },
-                    { x: 11, y: 4 },
-                    { x: 12, y: 4 },
-                    { x: 13, y: 4 },
-                    { x: 14, y: 4 },
-                    { x: 15, y: 4 },
-                    { x: 16, y: 4 },
-                    { x: 17, y: 4 },
-                    { x: 18, y: 4 },
-                    { x: 19, y: 4 },
-                    { x: 20, y: 4 },
-                    { x: 21, y: 4 },
-                    { x: 22, y: 4 },
-                    { x: 23, y: 4 },
-                    { x: 24, y: 4 },
-                    { x: 25, y: 7 }
-                  ]}
-              />
-            </VictoryChart>
-
-            <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-              <VictoryLine
-                  style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                  }}
-                  data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 4 },
-                    { x: 6, y: 4 },
-                    { x: 7, y: 4 },
-                    { x: 8, y: 4 },
-                    { x: 9, y: 4 },
-                    { x: 10, y: 4 },
-                    { x: 11, y: 4 },
-                    { x: 12, y: 4 },
-                    { x: 13, y: 4 },
-                    { x: 14, y: 4 },
-                    { x: 15, y: 4 },
-                    { x: 16, y: 4 },
-                    { x: 17, y: 4 },
-                    { x: 18, y: 4 },
-                    { x: 19, y: 4 },
-                    { x: 20, y: 4 },
-                    { x: 21, y: 4 },
-                    { x: 22, y: 4 },
-                    { x: 23, y: 4 },
-                    { x: 24, y: 4 },
-                    { x: 25, y: 7 }
-                  ]}
-              />
-            </VictoryChart>
-
-            <VictoryChart
-                theme={VictoryTheme.grayscale}
-            >
-              <VictoryLine
-                  style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                  }}
-                  data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 4 },
-                    { x: 6, y: 4 },
-                    { x: 7, y: 4 },
-                    { x: 8, y: 4 },
-                    { x: 9, y: 4 },
-                    { x: 10, y: 4 },
-                    { x: 11, y: 4 },
-                    { x: 12, y: 4 },
-                    { x: 13, y: 4 },
-                    { x: 14, y: 4 },
-                    { x: 15, y: 4 },
-                    { x: 16, y: 4 },
-                    { x: 17, y: 4 },
-                    { x: 18, y: 4 },
-                    { x: 19, y: 4 },
-                    { x: 20, y: 4 },
-                    { x: 21, y: 4 },
-                    { x: 22, y: 4 },
-                    { x: 23, y: 4 },
-                    { x: 24, y: 4 },
-                    { x: 25, y: 7 }
-                  ]}
-              />
-            </VictoryChart>
-
+            {/*<VictoryChart*/}
+            {/*    theme={VictoryTheme.grayscale}*/}
+            {/*>*/}
+            {/*  <VictoryLine*/}
+            {/*      style={{*/}
+            {/*        data: { stroke: "#c43a31" },*/}
+            {/*        parent: { border: "1px solid #ccc"}*/}
+            {/*      }}*/}
+            {/*      data={[*/}
+            {/*        { x: 1, y: 2 },*/}
+            {/*        { x: 2, y: 3 },*/}
+            {/*        { x: 3, y: 5 },*/}
+            {/*        { x: 4, y: 4 },*/}
+            {/*      ]}*/}
+            {/*  />*/}
+            {/*</VictoryChart>*/}
           </ScrollView>
         </SafeAreaView>
       </SafeAreaProvider>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 
 export default StatisticsPage;
