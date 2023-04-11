f459d789ac4c3d11008fed2d50d83b8a527f71e5
Galla <gallant.thomas@outlook.com>
2023-04-11 08:41:41 -0400

    saving changes

--- sudokuru/app/Pages/StatisticsPage.tsx
+++ sudokuru/app/Pages/StatisticsPage.tsx
@@ -54,6 +54,13 @@ const StatisticsPage = () => {
         }}>
           Delete Statistics
         </Button>
+        <LineChart width={500} height={300} data={data}>
+          <XAxis dataKey="name"/>
+          <YAxis/>
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+        </LineChart>
       </SafeAreaView>
     </SafeAreaProvider>
   );
