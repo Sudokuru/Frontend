cfc508b44f7c1b689762b5f12b4e597df523eed3
danielcosentino <daniel.cosentino@knights.ucf.edu>
2023-04-12 17:30:52 -0400

    fixed boxline reduction

--- sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
+++ sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
@@ -1062,6 +1062,9 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
       if (hint.removals) removals = getRemovalsFromHint(board, hint);
     }
 
+    let boxGroups = []
+    let nonBoxGroups = []
+
     let hintSteps = []
     switch (hint.strategy)
     {
@@ -1171,8 +1174,6 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
         hintSteps.push({}) // row/col and rem delete
 
         // seperate the groups which are boxes and which are not boxes
-        let boxGroups = []
-        let nonBoxGroups = []
         for (let i = 0; i < groups.length; i++)
           if (groups[i][0] === 2)
             boxGroups.push(groups[i])
@@ -1198,18 +1199,15 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
         for (let i = 0; i < removals.length; i++)
           hintSteps[2].removals.push({ ...removals[i], mode: "delete" });
         break;
-      case "BOX_LINE_REDUCTION":
-        console.log("Box Line Reduction");
+      case "BOX_LINE_REDUCTION": // DONE
         // three steps, three objects
         hintSteps.push({}) // box and causes
         hintSteps.push({}) // row/col and rem highlight
         hintSteps.push({}) // row/col and rem delete
 
         // seperate the groups which are boxes and which are not boxes
-        let boxGroups = []
-        let nonBoxGroups = []
         for (let i = 0; i < groups.length; i++)
-          if (groups[i][0] === 2)
+          if (groups[i].type == "box")
             boxGroups.push(groups[i])
           else
             nonBoxGroups.push(groups[i])
