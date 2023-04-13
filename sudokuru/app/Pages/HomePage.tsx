b83180288bc6add1b001984c104fb6810388aa1f
Galla <gallant.thomas@outlook.com>
2023-04-13 10:20:50 -0400

    Fixing amend notes bug and enabling window dimensions

--- sudokuru/app/Pages/HomePage.tsx
+++ sudokuru/app/Pages/HomePage.tsx
@@ -29,8 +29,8 @@ const HomePage = () => {
 
     const theme = useTheme();
 
-   // const size = useWindowDimensions();
-    const reSize = 30 //Math.min(size.width, size.height) / 25;
+    const size = useWindowDimensions();
+    const reSize = Math.min(size.width, size.height) / 25;
 
     const { updateLearnedLessons, learnedLessons } = React.useContext(PreferencesContext);
 
@@ -68,20 +68,6 @@ const HomePage = () => {
 
     useFocusEffect(
         React.useCallback(() => {
-            if(areLessonsLoaded){
-                if (!(learnedLessons.includes("SUDOKU_101"))) {
-                    navigation.navigate("Lesson" ,{params:'SUDOKU_101'});
-                }
-                else if (!(learnedLessons.includes("AMEND_NOTES"))) {
-                    navigation.navigate("Lesson" ,{params:'AMEND_NOTES'});
-                }
-                else if (!(learnedLessons.includes("NAKED_SINGLE"))) {
-                    navigation.navigate("Lesson" ,{params:'NAKED_SINGLE'});
-                }
-                else if (!(learnedLessons.includes("SIMPLIFY_NOTES"))) {
-                    navigation.navigate("Lesson" ,{params:'SIMPLIFY_NOTES'});
-                }
-            }
 
             // Show and display home content depending on canPlay function
             if (!canPlay(true)){
@@ -134,6 +120,22 @@ const HomePage = () => {
                         }
 
                         setLessonsLoaded(true);
+
+                        if(areLessonsLoaded){
+                            console.log("Hello!", learnedLessons)
+                            if (!(learnedLessons.includes("SUDOKU_101"))) {
+                                navigation.navigate("Lesson" ,{params:'SUDOKU_101'});
+                            }
+                            else if (!(learnedLessons.includes("AMEND_NOTES"))) {
+                                navigation.navigate("Lesson" ,{params:'AMEND_NOTES'});
+                            }
+                            else if (!(learnedLessons.includes("NAKED_SINGLE"))) {
+                                navigation.navigate("Lesson" ,{params:'NAKED_SINGLE'});
+                            }
+                            else if (!(learnedLessons.includes("SIMPLIFY_NOTES"))) {
+                                navigation.navigate("Lesson" ,{params:'SIMPLIFY_NOTES'});
+                            }
+                        }
                     }
                     else {
                         console.log("Error retrieving lessons of user");
