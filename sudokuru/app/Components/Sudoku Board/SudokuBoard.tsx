a3cdf28c8783113e46204df5bb7b1af474e9f578
Cosentino <daniel.cosentino@knights.ucf.edu>
2023-04-13 19:23:22 -0400

    hacky workaround, only renders the last two groups of pointing set

--- sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
+++ sudokuru/app/Components/Sudoku Board/SudokuBoard.tsx
@@ -1453,8 +1453,9 @@ export default class SudokuBoard extends React.Component<any, any, any, any, any
         hintSteps.push({}) // row/col and rem delete
 
         // seperate the groups which are boxes and which are not boxes
-        for (let i = 0; i < groups.length; i++)
-          if (groups[i][0] == "box")
+        print("groups", groups)
+        for (let i = groups.length - 2; i < groups.length; i++)
+          if (groups[i].type == "box")
             boxGroups.push(groups[i])
           else
             nonBoxGroups.push(groups[i])
