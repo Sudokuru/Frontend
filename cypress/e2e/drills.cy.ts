64f39837f9129112a5059c3cb709c74f187c7c9d
Galla <gallant.thomas@outlook.com>
2023-08-20 00:57:22 -0400

    Updating drill test and pipeline

--- cypress/e2e/drills.cy.ts
+++ cypress/e2e/drills.cy.ts
@@ -1,7 +1,15 @@
 describe("template spec", () => {
-  it("passes", () => {
+  beforeEach(() => {
+    window.localStorage.setItem(
+      "learned_lessons",
+      '["SUDOKU_101","AMEND_NOTES","NAKED_SINGLE","SIMPLIFY_NOTES","NAKED_SET","HIDDEN_SINGLE","HIDDEN_SET","POINTING_SET"]'
+    );
     cy.visit("");
+  });
+  it("passes", () => {
     cy.contains("Get Started").click();
     cy.contains("Start Drill").click();
+    cy.contains("Naked Sets").click();
+    cy.contains("Naked Pair").click();
   });
 });
