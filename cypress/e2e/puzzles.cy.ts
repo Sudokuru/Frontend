d53cb63337bcc6ba2b889f08bfd0095684855704
Galla <gallant.thomas@outlook.com>
2023-08-19 22:45:37 -0400

    Updating descriptions

--- cypress/e2e/puzzles.cy.ts
+++ cypress/e2e/puzzles.cy.ts
@@ -1,14 +1,14 @@
-describe("Pause Button functions for sudoku play component", () => {
+describe("Sudoku play component functions", () => {
   // Before tests insert learned lessons in localstorage
   before(() => {
     window.localStorage.setItem(
       "learned_lessons",
       '["SUDOKU_101","AMEND_NOTES","NAKED_SINGLE","SIMPLIFY_NOTES","NAKED_SET","HIDDEN_SINGLE","HIDDEN_SET","POINTING_SET"]'
     );
+    cy.visit("http://localhost:19000/");
   });
 
-  it("Then the user navigates to the Home page", () => {
-    cy.visit("http://localhost:19000/");
+  it("Pause Button functions for sudoku play component", () => {
     cy.contains("Get Started").click();
     cy.contains("Start Puzzle").click();
     cy.get("[data-testid=PauseButton]").click();
