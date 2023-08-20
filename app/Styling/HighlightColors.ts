45a7ed7b47b0fd0e4b1bfcdc801866719b7d744c
Gregory Travis <gregorytravis711@gmail.com>
2023-08-20 12:32:49 -0400

    Add RGB conversions to HighlightColors

--- app/Styling/HighlightColors.ts
+++ app/Styling/HighlightColors.ts
@@ -6,3 +6,29 @@ export const SELECTED_COLOR = "#9CC4EC";
 export const REMOVE_NOTE_TEXT_COLOR = "#FF0000";
 export const PLACE_NOTE_TEXT_COLOR = "#F2CA7E";
 export const NOT_HIGHLIGHTED_COLOR = "#FFFFFF";
+
+// from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRGB(hex: string) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+function RGB(hex: string):string {
+    const rgb = hexToRGB(hex);
+    if (rgb === null) {
+        throw new Error(`Invalid hex color: ${hex}`);
+    }
+    return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+}
+
+export const NOT_SELECTED_CONFLICT_COLOR_RGB = RGB(NOT_SELECTED_CONFLICT_COLOR);
+export const SELECTED_CONFLICT_COLOR_RGB = RGB(SELECTED_CONFLICT_COLOR);
+export const PEER_SELECTED_COLOR_RGB = RGB(PEER_SELECTED_COLOR);
+export const VALUE_SELECTED_COLOR_RGB = RGB(VALUE_SELECTED_COLOR);
+export const SELECTED_COLOR_RGB = RGB(SELECTED_COLOR);
+export const REMOVE_NOTE_TEXT_COLOR_RGB = RGB(REMOVE_NOTE_TEXT_COLOR);
+export const PLACE_NOTE_TEXT_COLOR_RGB = RGB(PLACE_NOTE_TEXT_COLOR);
+export const NOT_HIGHLIGHTED_COLOR_RGB = RGB(NOT_HIGHLIGHTED_COLOR);
