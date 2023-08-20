30795890dfd9466c7d2ca083c0e6de9626b262ee
Galla <gallant.thomas@outlook.com>
2023-08-20 10:44:14 -0400

    Updating page load timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:19000/",
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
