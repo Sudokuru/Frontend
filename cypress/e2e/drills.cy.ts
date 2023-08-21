b2356e1bf7c743c45b59d60d7b6213a42ae63181
Gregory Travis <gregorytravis711@gmail.com>
2023-08-20 22:31:05 -0400

    Finished Cypress test which finds naked single in drill and tests hint button

--- cypress/e2e/drills.cy.ts
+++ cypress/e2e/drills.cy.ts
@@ -1,4 +1,4 @@
-describe("template spec", () => {
+describe("naked single drills", () => {
   beforeEach(() => {
     window.localStorage.setItem(
       "learned_lessons",
@@ -6,13 +6,15 @@ describe("template spec", () => {
     );
     cy.visit("");
   });
-  it("passes", () => {
+  it("finds naked single in drill and hint is correctly applied", () => {
     cy.contains("Get Started").click();
     cy.contains("Start Drill").click();
     cy.contains("Naked Sets").click();
     cy.get('[data-testid="Naked Single"]').click();
-    cy.log("test");
-    let singleCount = 0;
+    let singleCount = 0,
+      singleRow = 0,
+      singleCol = 0;
+    let note = "";
     cy.get("[data-testid=sudokuBoard]")
       .within(() => {
         for (let i = 0; i < 9; i++) {
@@ -21,12 +23,14 @@ describe("template spec", () => {
               .invoke("data", "testid")
               .then((cellId) => {
                 let cellIdString = cellId.toString();
-                cy.log(cellIdString);
                 if (cellIdString.includes("notes:")) {
                   let notesIndex = cellIdString.indexOf("notes:");
                   let notesString = cellIdString.substring(notesIndex + 6);
                   if (notesString.length === 1) {
                     singleCount++;
+                    singleRow = i;
+                    singleCol = j;
+                    note = notesString;
                   }
                 }
               });
@@ -35,6 +39,22 @@ describe("template spec", () => {
       })
       .then(() => {
         expect(singleCount).to.equal(1);
+        cy.get("[data-testid=hintButton]")
+          .click()
+          .get("[data-testid=rightArrow]")
+          .click()
+          .get("[data-testid=checkMark]")
+          .click()
+          .get(
+            "[data-testid=cellr" +
+              singleRow +
+              "c" +
+              singleCol +
+              "value\\:" +
+              note +
+              "]"
+          )
+          .should("exist");
       });
   });
 });
