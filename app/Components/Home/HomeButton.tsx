8951b3164702ca80f4f6912f4a0ef79bb0d7629f
Galla <gallant.thomas@outlook.com>
2023-08-26 17:14:57 -0400

    Fixing navigation bug

--- app/Components/Home/HomeButton.tsx
+++ app/Components/Home/HomeButton.tsx
@@ -2,17 +2,20 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Button, useTheme } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
+import { PreferencesContext } from "../../Contexts/PreferencesContext";
 
 const HomeButton = () => {
   const navigation: any = useNavigation();
   const theme = useTheme();
+
+  const { isCurrentPage } = React.useContext(PreferencesContext);
 
   return (
     <Button
       mode="contained"
       testID={"ViewHomePageButton"}
       style={{ marginRight: 5 }}
-      onPress={() => navigation.navigate("Landing")}
+      onPress={() => navigation.navigate(isCurrentPage)}
     >
       <MaterialCommunityIcons
         name="home"
