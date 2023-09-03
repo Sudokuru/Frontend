fb114bedbc8cb468ea8b788d021bdcf600a18f55
Galla <gallant.thomas@outlook.com>
2023-09-03 14:18:21 -0400

    adding jest config file and jest-junit reporter

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "jest-expo",
+  reporters: ["default", "github-actions", "jest-junit"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  collectCoverage: false,
+  coverageDirectory: "jest-coverage",
+};
+
+export default config;
