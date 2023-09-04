9007d4710a456b791951ef6c58286eea1bc52b49
Gregory Travis <gregorytravis711@gmail.com>
2023-09-04 12:05:23 -0400

    Added support for removals to Hint class

--- app/Components/Sudoku Board/Functions/Hint.ts
+++ app/Components/Sudoku Board/Functions/Hint.ts
@@ -7,6 +7,7 @@ export default class Hint {
   private stepCount: number;
   private groups: Group[][];
   private causes: Group[][];
+  private removals: Group[][];
 
   /**
    * Creates a hint object
@@ -16,9 +17,11 @@ export default class Hint {
     this.stepCount = stepCount;
     this.groups = [];
     this.causes = [];
+    this.removals = [];
     for (let i: number = 0; i < stepCount; i++) {
       this.groups.push([]);
       this.causes.push([]);
+      this.removals.push([]);
     }
   }
 
@@ -32,8 +35,8 @@ export default class Hint {
   }
 
   /**
-   * Adds a cause to the hint
-   * @param step - step to add the cause to
+   * Adds causes to the hint
+   * @param step - step to add the causes to
    * @param causes - causes to add
    */
   public addCauses(step: number, causes: number[][]): void {
@@ -43,6 +46,20 @@ export default class Hint {
       tempGroup.setCol(causes[i][1]);
       this.causes[step].push(tempGroup);
     }
+  }
+
+  /**
+   * Adds a removal to the hint
+   * @param step - step to add the removal to
+   * @param position - position of the removal
+   * @param values - values of the removal
+   */
+  public addRemoval(step: number, position: number[], values: number[]): void {
+    let tempGroup: Group = new Group();
+    tempGroup.setRow(position[0]);
+    tempGroup.setCol(position[1]);
+    tempGroup.setValues(values);
+    this.removals[step].push(tempGroup);
   }
 
   /**
@@ -61,6 +78,18 @@ export default class Hint {
         hintSteps[step].causes = [];
         for (let cause: number = 0; cause < this.causes[step].length; cause++) {
           hintSteps[step].causes.push(this.causes[step][cause].getCause());
+        }
+      }
+      if (this.removals[step].length > 0) {
+        hintSteps[step].removals = [];
+        for (
+          let removal: number = 0;
+          removal < this.removals[step].length;
+          removal++
+        ) {
+          hintSteps[step].removals.push(
+            this.removals[step][removal].getRemoval()
+          );
         }
       }
     }
