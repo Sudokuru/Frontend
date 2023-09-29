4a83d74f97ae8333a55770b8a0b109d039f01953
Thomas Gallant <60023923+Thomas-Gallant@users.noreply.github.com>
2023-09-28 20:19:28 -0400

    Removing unused code

--- app/Components/Sudoku Board/sudoku.ts
+++ app/Components/Sudoku Board/sudoku.ts
@@ -1,10 +1,6 @@
 // @ts-nocheck
 import { fromJS } from "immutable";
 
-function randomChoice(choices) {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
-
 export function range(n) {
   return Array.from(Array(n).keys());
 }
@@ -44,48 +40,6 @@ export function makeBoard({ puzzle }, initialPuzzle) {
 }
 
 /**
- * Answers the question: can the cell (i,j) in the puzzle contain the number
- in cell "c"
- * @param puzzle
- * @param i
- * @param j
- * @param c
- */
-function canBeA(puzzle, i, j, c) {
-  const x = Math.floor(c / 9);
-  const y = c % 9;
-  const value = puzzle[x][y];
-  if (puzzle[i][j] === value) return true;
-  if (puzzle[i][j] > 0) return false;
-  // if not the cell itself, and the mth cell of the group contains the value v, then "no"
-  // eslint-disable-next-line guard-for-in,no-restricted-syntax
-  for (const m in Array.from(Array(9).keys())) {
-    const rowPeer = { x: m, y: j };
-    const columnPeer = { x: i, y: m };
-    const SquarePeer = {
-      x: Math.floor(i / 3) * 3 + Math.floor(m / 3),
-      y: Math.floor(j / 3) * 3 + (m % 3),
-    };
-    if (
-      !(rowPeer.x === x && rowPeer.y === y) &&
-      puzzle[(rowPeer.x, rowPeer.y)] === value
-    )
-      return false;
-    if (
-      !(columnPeer.x === x && columnPeer.y === y) &&
-      puzzle[(columnPeer.x, columnPeer.y)] === value
-    )
-      return false;
-    if (
-      !(SquarePeer.x === x && SquarePeer.y === y) &&
-      puzzle[(SquarePeer.x, SquarePeer.y)] === value
-    )
-      return false;
-  }
-  return true;
-}
-
-/**
  *
  * @param a
  * @param b
@@ -110,63 +64,4 @@ export function highlightRow(a, b) {
 
 export function highlightColumn(a, b) {
   return a.x === b.x;
-}
-
-export function pluck(allCells, n = 0) {
-  const puzzle = JSON.parse(JSON.stringify(allCells));
-  /**
-   * starts with a set of all 81 cells, and tries to remove one (randomly) at a time,
-   * but not before checking that the cell can still be deduced from the remaining cells.
-   * @type {Set}
-   */
-  const cells = new Set(Array.from(Array(81).keys()));
-  const cellsLeft = new Set(cells);
-  while (cellsLeft.size && cells.size > n) {
-    const cell = randomChoice([...cells]);
-    const x = Math.floor(cell / 9);
-    const y = cell % 9;
-    cellsLeft.delete(cell);
-    /**
-     * row, column and square record whether another cell in those groups could also take
-     * on the value we are trying to pluck. (If another cell can, then we can't use the
-     * group to deduce this value.) If all three groups are True, then we cannot pluck
-     * this cell and must try another one.
-     */
-    let row = false;
-    let column = false;
-    let square = false;
-    range(9).forEach((i) => {
-      const rowPeer = { x: i, y };
-      const columnPeer = { x, y: i };
-      const squarePeer = {
-        x: Math.floor(Math.floor(cell / 9) / 3) * 3 + Math.floor(i / 3),
-        y: (Math.floor(cell / 9) % 3) * 3 + (i % 3),
-      };
-      if (rowPeer.x !== x) {
-        row = canBeA(puzzle, rowPeer.x, rowPeer.y, cell);
-      }
-      if (columnPeer.y !== y) {
-        column = canBeA(puzzle, columnPeer.x, columnPeer.y, cell);
-      }
-      if (squarePeer.x !== x && squarePeer.y !== y) {
-        square = canBeA(puzzle, squarePeer.x, squarePeer.y, cell);
-      }
-    });
-    if (row && column && square) {
-      // eslint-disable-next-line no-continue
-      continue;
-    } else {
-      // this is a pluckable cell!
-      // eslint-disable-next-line no-param-reassign
-      puzzle[x][y] = 0; // 0 denotes a blank cell
-      /**
-       * remove from the set of visible cells (pluck it)
-       * we don't need to reset "cellsleft" because if a cell was not pluckable
-       * earlier, then it will still not be pluckable now (with less information
-       * on the board).
-       */
-      cells.delete(cell);
-    }
-  }
-  return { puzzle, size: cells.size };
 }
