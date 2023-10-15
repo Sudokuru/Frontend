21bc80798f1afb396f53274f4fae79f94a0947d5
Gregory Travis <gregorytravis711@gmail.com>
2023-10-15 10:46:33 -0400

    Added comments to grayscale script

--- app/Components/Home/CardImages/to_grayscale.sh
+++ app/Components/Home/CardImages/to_grayscale.sh
@@ -3,9 +3,10 @@
 # Get a list of all files in the current directory
 files=$(ls)
 
-# Iterate over the list of files and echo the filename of each file that ends in .png
+# Iterate over the list of files
 for file in $files; do
   # Check if the file is a regular file and if the filename ends in .png
+  # If both conditions are true, convert the file to grayscale (using ImageMagick)
   if [[ -f $file && "$file" =~ \.png$ ]]; then
     convert $file -colorspace Gray "Learned/$file"
   fi
