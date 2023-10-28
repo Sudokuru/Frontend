3adcc6e5b39cbb72837afde5d0238ae70641acc7
Gregory Travis <gregorytravis711@gmail.com>
2023-10-28 11:08:12 -0400

    Removed nested view

--- app/Pages/Lesson.tsx
+++ app/Pages/Lesson.tsx
@@ -139,32 +139,30 @@ const Lesson = (props: { route: { params: { params: any } } }) => {
   return (
     <ScrollView>
       <SafeAreaView style={{ height: "100%", width: "100%" }}>
-        <View>
-          <View style={styles.container1}>
-            <View style={{ flexDirection: "row" }}>
-              <Text
-                style={{
-                  color: theme.colors.primary,
-                  fontSize: reSize / 25,
-                  fontWeight: "bold",
-                }}
-              >
-                {title + " Lesson"}
-              </Text>
-              <Pressable onPress={() => showLearnHelp()}>
-                <MaterialCommunityIcons
-                  color={theme.colors.onBackground}
-                  name="help"
-                />
-              </Pressable>
-            </View>
+        <View style={styles.container1}>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              style={{
+                color: theme.colors.primary,
+                fontSize: reSize / 25,
+                fontWeight: "bold",
+              }}
+            >
+              {title + " Lesson"}
+            </Text>
+            <Pressable onPress={() => showLearnHelp()}>
+              <MaterialCommunityIcons
+                color={theme.colors.onBackground}
+                name="help"
+              />
+            </Pressable>
+          </View>
 
-            <View style={styles.container1}>
-              {cards}
-              <Button onPress={() => clickCheckMark()}>
-                Mark Lesson as Complete
-              </Button>
-            </View>
+          <View style={styles.container1}>
+            {cards}
+            <Button onPress={() => clickCheckMark()}>
+              Mark Lesson as Complete
+            </Button>
           </View>
         </View>
         <Alert
