46083457f9214054a275155d280b3a88ef89c74a
Galla <gallant.thomas@outlook.com>
2024-01-28 17:08:49 -0500

    Adding conversion functions

--- app/Components/Sudoku Board/Functions/HintFunctions.ts
+++ app/Components/Sudoku Board/Functions/HintFunctions.ts
@@ -1,1 +1,29 @@
-export const getHint = () => {};
+import { getHint } from "sudokuru";
+
+export const getSudokuHint = () => {
+  getHint();
+};
+
+const convertPuzzleSolutionToSudokuruFormat = (
+  puzzleSolution: number[][]
+): string[][] => {
+  const convertedPuzzleSolution: string[][] = [];
+  puzzleSolution.forEach(function (value, index) {
+    convertedPuzzleSolution.push([]);
+    convertedPuzzleSolution[index] = value.map(String);
+  });
+  return convertedPuzzleSolution;
+};
+
+const convertPuzzleSolutionToFrontendFormat = (puzzleSolution: string[][]) => {
+  const convertedPuzzleSolution: number[][] = [];
+  puzzleSolution.forEach(function (value, index) {
+    convertedPuzzleSolution.push([]);
+    convertedPuzzleSolution[index] = value.map(Number);
+  });
+  return convertedPuzzleSolution;
+};
+
+const convertPuzzleStateToSudokuruFormat = () => {};
+
+const convertPuzzleStateToFrontendFormat = () => {};
