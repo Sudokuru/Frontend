95e470f2200d26ae00fc7c0053ac83828ae5fb01
Gregory Travis <gregorytravis711@gmail.com>
2024-02-04 11:48:27 -0500

    Added stack navigator for Contact page

---
+++ app/Navigation/StackNavigators/ContactStackNavigator.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import ContactPage from "../../Pages/ContactPage";
+
+const Stack = createStackNavigator();
+
+const ContactStackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        title: "Sudokuru",
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="ContactPage" component={ContactPage} />
+    </Stack.Navigator>
+  );
+};
+
+export default ContactStackNavigator;
