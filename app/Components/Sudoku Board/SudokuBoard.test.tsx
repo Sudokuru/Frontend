58276e12896788f410f71eb4df4b3401c8d2a8bd
Galla <gallant.thomas@outlook.com>
2024-02-10 22:48:16 -0500

    saving progress on test

--- app/Components/Sudoku Board/SudokuBoard.test.tsx
+++ app/Components/Sudoku Board/SudokuBoard.test.tsx
@@ -4,6 +4,7 @@ import {
   fireEvent,
   act,
   waitFor,
+  userEvent,
 } from "@testing-library/react-native";
 import SudokuBoard from "./SudokuBoard";
 
@@ -11,4 +12,25 @@ test("Testing Board Renders", async () => {
   const board = await waitFor(() => render(<SudokuBoard action="StartGame" />));
 
   expect(board).toBeTruthy();
+});
+
+test("Testing Board Renders", async () => {
+  const board = await waitFor(() => render(<SudokuBoard action="StartGame" />));
+
+  const emptyCell = screen.queryByTestId("cellr0c0value:0");
+  const filledCell = screen.queryByTestId("cellr0c0value:1");
+
+  expect(emptyCell).toBeTruthy();
+  expect(filledCell).toBeFalsy();
+
+  await act(async () => {
+    await fireEvent.press(emptyCell);
+    await fireEvent.changeText(emptyCell, "1");
+  });
+
+  const newEmptyCell = screen.queryByTestId("cellr0c0value:0");
+  const newFilledCell = screen.queryByTestId("cellr0c0value:1");
+
+  expect(newEmptyCell).toBeTruthy();
+  expect(newFilledCell).toBeTruthy();
 });
