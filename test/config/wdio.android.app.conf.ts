35ffdc5de96c6d42d3bf10f53f42f5f1c438622e
Galla <gallant.thomas@outlook.com>
2024-02-11 20:10:14 -0500

    saving progress

---
+++ test/config/wdio.android.app.conf.ts
@@ -0,0 +1,40 @@
+import { join } from "node:path";
+import { config as baseConfig } from "./wdio.shared.local.appium.conf";
+
+export const config: WebdriverIO.Config = {
+  ...baseConfig,
+
+  // ============
+  // Capabilities
+  // ============
+  // For all capabilities please check
+  // https://github.com/appium/appium-uiautomator2-driver
+  capabilities: [
+    {
+      // The defaults you need to have in your config
+      platformName: "Android",
+      maxInstances: 1,
+      // For W3C the appium capabilities need to have an extension prefix
+      // This is `appium:` for all Appium Capabilities which can be found here
+
+      //
+      // NOTE: Change this name according to the Emulator you have created on your local machine
+      "appium:deviceName": "Pixel_7_Pro_Android_14_API_34",
+      //
+      // NOTE: Change this version according to the Emulator you have created on your local machine
+      "appium:platformVersion": "14.0",
+      "appium:orientation": "PORTRAIT",
+      "appium:automationName": "UiAutomator2",
+      // The path to the app
+      "appium:app": join(
+        process.cwd(),
+        "apps",
+        //
+        // NOTE: Change this name according to the app version you downloaded
+        "android.wdio.native.app.v1.0.8.apk"
+      ),
+      "appium:appWaitActivity": "com.wdiodemoapp.MainActivity",
+      "appium:newCommandTimeout": 240,
+    },
+  ],
+};
---
+++ test/config/wdio.shared.local.appium.conf.ts
@@ -0,0 +1,38 @@
+import { config as baseConfig } from "../../wdio.conf";
+
+export const config: WebdriverIO.Config = {
+  ...baseConfig,
+
+  //
+  // ======
+  // Appium
+  // ======
+  //
+  services: [
+    ...(baseConfig.services || []),
+    [
+      "appium",
+      {
+        // This will use the globally installed version of Appium
+        // command: 'appium',
+        args: {
+          // This is needed to tell Appium that we can execute local ADB commands
+          // and to automatically download the latest version of ChromeDriver
+          relaxedSecurity: true,
+          // Write the Appium logs to a file in the root of the directory
+          log: "./logs/appium.log",
+        },
+      },
+    ],
+  ],
+  before: async () => {
+    // Only update the setting for Android, this is needed to reduce the timeout for the UiSelector locator strategy,
+    // which is also used in certain tests, so it will not wait for 10 seconds if it can't find an element
+    if (driver.isAndroid) {
+      await driver.updateSettings({
+        // This reduces the timeout for the UiUiSelector from 10 seconds to 3 seconds
+        waitForSelectorTimeout: 3 * 1000,
+      });
+    }
+  },
+};
