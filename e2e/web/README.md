a229b796bcc8d2c1ddff3e9a1311fc00df86a838
Galla <gallant.thomas@outlook.com>
2024-02-12 17:46:51 -0500

    updating readme and adding back chrome tests

--- e2e/web/README.md
+++ e2e/web/README.md
@@ -6,5 +6,6 @@
 
 # Run Tests
 
+- make sure that the website is running locally (or change baseURL to match where you want to test)
 - run `npm run ui` to run tests using playwright ui
 - run `npm run test` to run tests using playwright cli
--- e2e/web/playwright.config.ts
+++ e2e/web/playwright.config.ts
@@ -44,10 +44,10 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chrome"] },
-    // },
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
