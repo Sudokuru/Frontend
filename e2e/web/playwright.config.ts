5c66612fa133e8e8966f30739548cb8825399650
Galla <gallant.thomas@outlook.com>
2024-02-13 19:28:04 -0500

    uncommenting out all browser versions

--- e2e/web/playwright.config.ts
+++ e2e/web/playwright.config.ts
@@ -46,44 +46,38 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      timeout: 600000,
       use: {
         ...devices["Desktop Chrome"],
-        // launchOptions: {
-        //   headless: false,
-        //   args: ["--no-incognito"],
-        // },
       },
     },
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: "Mobile Chrome",
-    //   use: { ...devices["Pixel 5"] },
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPhone 12"] },
-    // },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
+    },
 
     /* Test against branded browsers. */
-    // {
-    //   name: "Microsoft Edge",
-    //   use: { ...devices["Desktop Edge"], channel: "msedge" },
-    // },
-    // {
-    //   name: "Google Chrome",
-    //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
-    // },
+    {
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" },
+    },
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
