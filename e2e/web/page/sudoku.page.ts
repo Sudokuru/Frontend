1c998ddd0b44b3528cb66019cef98f131c440d1c
Gregory Travis <gregorytravis711@gmail.com>
2024-03-02 16:44:36 -0500

    Added pause button to playwright sudoku page

--- e2e/web/page/sudoku.page.ts
+++ e2e/web/page/sudoku.page.ts
@@ -3,10 +3,12 @@ import { Locator, Page, expect } from "@playwright/test";
 export class SudokuPage {
   readonly page: Page;
   readonly timer: Locator;
+  readonly pause: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.timer = page.getByText("Time: ");
+    this.pause = page.getByTestId("PauseButton");
   }
 
   async sudokuPageIsRendered() {
