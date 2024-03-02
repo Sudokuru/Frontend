f707f78a509286cba684178437dafd303ca674cb
Gregory Travis <gregorytravis711@gmail.com>
2024-03-02 16:42:35 -0500

    Added resume game fixture

--- e2e/web/fixture.ts
+++ e2e/web/fixture.ts
@@ -1,4 +1,7 @@
 import { Page, test as base } from "@playwright/test";
+import { ACTIVE_GAME } from "../../cypress/global/testIds";
+import { HomePage } from "./page/home.page";
+import { PlayPage } from "./page/play.page";
 
 // Declare the types of your fixtures.
 type MyFixtures = {
@@ -10,6 +13,22 @@ type MyFixtures = {
 export const test = base.extend<MyFixtures>({
   page: async ({ page }, use) => {
     await page.goto("");
+    await use(page);
+  },
+});
+
+// Loads a game from local storage and navigates to resume the game.
+export const resumeGame = base.extend<MyFixtures>({
+  page: async ({ page }, use) => {
+    await page.goto("");
+    await page.evaluate(() => {
+      window.localStorage.setItem("active_game", ACTIVE_GAME);
+    });
+    const homePage = new HomePage(page);
+    await homePage.playSudoku.click();
+    const playPage = new PlayPage(page);
+    await playPage.page.getByText("Resume Puzzle").click();
+    await playPage.playPageIsRendered();
     await use(page);
   },
 });
