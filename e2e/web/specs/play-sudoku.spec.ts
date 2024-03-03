f2c66a7cd7f2f6ef3b0af23987a5fe7f681f1872
Gregory Travis <gregorytravis711@gmail.com>
2024-03-03 18:27:35 -0500

    Added another expect to end of board highlight test so can see what is going on in ui

--- e2e/web/specs/play-sudoku.spec.ts
+++ e2e/web/specs/play-sudoku.spec.ts
@@ -47,6 +47,7 @@ resumeGame.describe("board highlighting", () => {
           }
         }
       }
+      await sudokuBoard.cell[7][7].click(); // without this cannot see for loop expect snapshots in ui
     }
   );
 });
