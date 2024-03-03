1c0425827a487da28c5f58873c0f80e23b205cc2
Gregory Travis <gregorytravis711@gmail.com>
2024-03-03 18:05:58 -0500

    Started board highlighting test using new cell color function

--- e2e/web/specs/play-sudoku.spec.ts
+++ e2e/web/specs/play-sudoku.spec.ts
@@ -2,6 +2,7 @@ import { resumeGame } from "../fixture";
 import { expect } from "@playwright/test";
 import { PlayPage } from "../page/play.page";
 import { SudokuBoardComponent } from "../components/sudoku-board.component";
+import { NOT_SELECTED_CONFLICT_COLOR_RGB } from "../../../app/Styling/HighlightColors";
 
 resumeGame.describe("special sudoku buttons", () => {
   resumeGame("pause button", async ({ page }) => {
@@ -10,4 +11,15 @@ resumeGame.describe("special sudoku buttons", () => {
     const playPage = new PlayPage(page);
     await playPage.playPageIsRendered();
   });
+});
+
+resumeGame.describe("board highlighting", () => {
+  resumeGame(
+    "highlighting should render correctly when a cell is selected",
+    async ({ page }) => {
+      const sudokuBoard = new SudokuBoardComponent(page);
+      await sudokuBoard.cellHasColor(7, 6, NOT_SELECTED_CONFLICT_COLOR_RGB);
+      // todo: finish this test
+    }
+  );
 });
