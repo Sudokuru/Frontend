0ec76cdfb22d1b95a64fa7386fedc20c7c145f6c
Gregory Travis <gregorytravis711@gmail.com>
2024-03-20 22:48:00 -0400

    Added to playwright contact page

--- e2e/web/page/contact.page.ts
+++ e2e/web/page/contact.page.ts
@@ -1,12 +1,16 @@
 import { Locator, Page, expect } from "@playwright/test";
 
 export class ContactPage {
+  readonly page: Page;
   readonly title: Locator;
   readonly submitFeedback: Locator;
+  readonly feedback: Locator;
 
   constructor(page: Page) {
+    this.page = page;
     this.title = page.getByText("Contact Us");
     this.submitFeedback = page.getByTestId("SubmitFeedbackButton");
+    this.feedback = page.getByTestId("FeedbackTextInput");
   }
 
   async contactPageIsRendered() {
@@ -15,5 +19,9 @@ export class ContactPage {
 
   async submitFeedbackButtonIsDisabled() {
     await expect(this.submitFeedback).toBeDisabled();
+  }
+
+  async submitFeedbackButtonIsEnabled() {
+    await expect(this.submitFeedback).toBeEnabled();
   }
 }
