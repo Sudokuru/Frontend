ef728084fe85eb8748a4fd2f8e9d833aac120fa8
Galla <gallant.thomas@outlook.com>
2024-03-24 01:38:38 -0400

    Adding dev plugins

--- app/App.tsx
+++ app/App.tsx
@@ -3,18 +3,26 @@ import "react-native-gesture-handler"; // This needs to be at top to work
 import * as React from "react";
 import { PreferencesContext } from "./Contexts/PreferencesContext";
 import { Provider as PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import InitializeContext from "./Contexts/InitializeContext";
 import DrawerNavigator from "./Navigation/DrawerNavigator";
 import { registerRootComponent } from "expo";
+import { useAsyncStorageDevTools } from "@dev-plugins/async-storage";
+import { useReactNavigationDevTools } from "@dev-plugins/react-navigation";
 
 function App() {
   const { theme, preferences } = InitializeContext();
+  useAsyncStorageDevTools();
+  const navigationRef = useNavigationContainerRef();
+  useReactNavigationDevTools(navigationRef);
 
   return (
     <PreferencesContext.Provider value={preferences}>
       <PaperProvider theme={theme}>
-        <NavigationContainer theme={theme}>
+        <NavigationContainer theme={theme} ref={navigationRef}>
           <DrawerNavigator />
         </NavigationContainer>
       </PaperProvider>
