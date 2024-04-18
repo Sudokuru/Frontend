9247da4b0d85ec411102b2be560e197fb729bbc7
Gregory Travis <gregorytravis711@gmail.com>
2024-04-18 19:22:40 -0400

    Trying differ border size on hovering

--- app/Components/Home/NavigationButton.tsx
+++ app/Components/Home/NavigationButton.tsx
@@ -42,7 +42,7 @@ const NavigationButton = (props: navigationButton) => {
               borderColor: hovered
                 ? theme.colors.outline
                 : theme.colors.onSurface,
-              borderWidth: 4,
+              borderWidth: hovered ? 4 : 2,
             }}
             testID={props.testID}
           >
