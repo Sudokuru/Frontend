e5fd461bbb2085ddf1ffb1b3229158bbd506c6f5
Galla <gallant.thomas@outlook.com>
2024-04-21 00:05:17 -0400

    helper functions to reduce duplicate code

--- e2e/web/specs/hints.spec.ts
+++ e2e/web/specs/hints.spec.ts
@@ -13,19 +13,17 @@ import {
   NOT_HIGHLIGHTED_COLOR_RGB,
 } from "../../../app/Styling/HighlightColors";
 
+// todo test that board is unselected when entering hint mode
+
+// todo test that board cannot be selected when entering hint mode
+
+// todo test hotkeys for hint mode
+
+// todo test all variants simplify notes - row, column, box
+
+// todo test hidden single
+
 test.describe("board AMEND_NOTES", () => {
-  // todo test that board is unselected when entering hint mode
-
-  // todo test that board cannot be selected when entering hint mode
-
-  // todo test hotkeys for hint mode
-
-  // todo test all variants amend notes - empty cell, cell with incorrect values, cell with correct values, cell with incorrect and correct values
-
-  // todo test all variants simplify notes - row, column, box
-
-  // todo test hidden single
-
   test.use({ gameToResume: AMEND_NOTES_EMPTY_CELL_GAME });
 
   test("with empty cell and 3 groups", async ({ resumeGame }) => {
@@ -47,81 +45,17 @@ test.describe("board AMEND_NOTES", () => {
     };
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
-    await sudokuBoard.hint.click();
 
-    await sudokuBoard.sudokuBoardContainsText("Amend Notes");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.sudokuBoardContainsText(
-      "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+    await amendNotesBaseTest(
+      sudokuBoard,
+      0,
+      0,
+      hintSelectedColor,
+      notHighlightedColor,
+      "0",
+      "1",
+      "1"
     );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.sudokuBoardContainsText(
-      "The hint is located in this region"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    // testing undo logic
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasValue(0, 0, "0");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 0, "1");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 0, "1");
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.hintFinish.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
-    ]);
-
-    await sudokuBoard.cellHasNotes(0, 0, "1");
-
-    // testing undo works at end of hint
-    await sudokuBoard.undo.click();
-    await sudokuBoard.cellHasValue(0, 0, "0");
   });
 });
 
@@ -144,92 +78,17 @@ test.describe("board AMEND_NOTES", () => {
     };
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
-    await sudokuBoard.hint.click();
 
-    await sudokuBoard.sudokuBoardContainsText("Amend Notes");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.sudokuBoardContainsText(
-      "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+    await amendNotesBaseTest(
+      sudokuBoard,
+      0,
+      5,
+      hintSelectedColor,
+      notHighlightedColor,
+      "27",
+      "125678",
+      "1568"
     );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.sudokuBoardContainsText(
-      "The hint is located in this region"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    // testing undo logic
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 5, "27");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 5, "125678");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 5, "1568");
-
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 5, "125678");
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 5, "27");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 5, "125678");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 5, "1568");
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.hintFinish.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
-    ]);
-
-    await sudokuBoard.cellHasNotes(0, 5, "1568");
-
-    // testing undo works at end of hint
-    await sudokuBoard.undo.click();
-    await sudokuBoard.cellHasNotes(0, 5, "27");
   });
 });
 
@@ -253,81 +112,17 @@ test.describe("board AMEND_NOTES", () => {
     };
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
-    await sudokuBoard.hint.click();
 
-    await sudokuBoard.sudokuBoardContainsText("Amend Notes");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.sudokuBoardContainsText(
-      "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+    await amendNotesBaseTest(
+      sudokuBoard,
+      0,
+      6,
+      hintSelectedColor,
+      notHighlightedColor,
+      "25",
+      "25689",
+      "25689"
     );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.sudokuBoardContainsText(
-      "The hint is located in this region"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    // testing undo logic
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 6, "25");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 6, "25689");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 6, "25689");
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.hintFinish.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
-    ]);
-
-    await sudokuBoard.cellHasNotes(0, 6, "25689");
-
-    // testing undo works at end of hint
-    await sudokuBoard.undo.click();
-    await sudokuBoard.cellHasNotes(0, 6, "25");
   });
 });
 
@@ -353,94 +148,129 @@ test.describe("board AMEND_NOTES", () => {
     };
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
-    await sudokuBoard.hint.click();
 
-    await sudokuBoard.sudokuBoardContainsText("Amend Notes");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.sudokuBoardContainsText(
-      "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+    await amendNotesBaseTest(
+      sudokuBoard,
+      0,
+      8,
+      hintSelectedColor,
+      notHighlightedColor,
+      "124",
+      "12489",
+      "289"
     );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.sudokuBoardContainsText(
-      "The hint is located in this region"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    // testing undo logic
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 8, "124");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 8, "12489");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 8, "289");
-
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 8, "12489");
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 8, "124");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 8, "12489");
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasNotes(0, 8, "289");
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => hintSelectedColor(row, column),
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => notHighlightedColor(row, column),
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.hintFinish.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
-    ]);
-
-    await sudokuBoard.cellHasNotes(0, 8, "289");
-
-    // testing undo works at end of hint
-    await sudokuBoard.undo.click();
-    await sudokuBoard.cellHasNotes(0, 8, "124");
   });
 });
+
+const amendNotesBaseTest = async (
+  sudokuBoard: SudokuBoardComponent,
+  row: number,
+  column: number,
+  hintSelectedColor: (row: number, column: number) => boolean,
+  notHighlightedColor: (row: number, column: number) => boolean,
+  initialCellNotes: string,
+  stageFourCellNotes: string,
+  stageFiveCellNotes: string
+) => {
+  await sudokuBoard.hint.click();
+
+  await sudokuBoard.sudokuBoardContainsText("Amend Notes");
+  await sudokuBoard.hintArrowRight.click();
+  await sudokuBoard.sudokuBoardContainsText(
+    "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+  );
+  await sudokuBoard.hintArrowRight.click();
+
+  await sudokuBoard.isSudokuBoardHighlightedCorrectly([
+    {
+      condition: (row, column) => notHighlightedColor(row, column),
+      color: NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+    {
+      condition: (row, column) => true,
+      color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+  ]);
+
+  await sudokuBoard.sudokuBoardContainsText(
+    "The hint is located in this region"
+  );
+  await sudokuBoard.hintArrowRight.click();
+
+  await sudokuBoard.isSudokuBoardHighlightedCorrectly([
+    {
+      condition: (row, column) => hintSelectedColor(row, column),
+      color: HINT_SELECTED_COLOR_RGB,
+    },
+    {
+      condition: (row, column) => notHighlightedColor(row, column),
+      color: NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+    {
+      condition: (row, column) => true,
+      color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+  ]);
+
+  // testing undo logic
+  await sudokuBoard.hintArrowLeft.click();
+  if (initialCellNotes === "0") {
+    await sudokuBoard.cellHasValue(row, column, initialCellNotes);
+  } else {
+    await sudokuBoard.cellHasNotes(row, column, initialCellNotes);
+  }
+  await sudokuBoard.hintArrowRight.click();
+  await sudokuBoard.cellHasNotes(row, column, stageFourCellNotes);
+
+  await sudokuBoard.hintArrowRight.click();
+  await sudokuBoard.cellHasNotes(row, column, stageFiveCellNotes);
+
+  await sudokuBoard.hintArrowLeft.click();
+  await sudokuBoard.cellHasNotes(row, column, stageFourCellNotes);
+  await sudokuBoard.hintArrowLeft.click();
+  if (initialCellNotes === "0") {
+    await sudokuBoard.cellHasValue(row, column, initialCellNotes);
+  } else {
+    await sudokuBoard.cellHasNotes(row, column, initialCellNotes);
+  }
+
+  await sudokuBoard.hintArrowRight.click();
+  await sudokuBoard.cellHasNotes(row, column, stageFourCellNotes);
+
+  await sudokuBoard.hintArrowRight.click();
+  await sudokuBoard.cellHasNotes(row, column, stageFiveCellNotes);
+
+  await sudokuBoard.isSudokuBoardHighlightedCorrectly([
+    {
+      condition: (row, column) => hintSelectedColor(row, column),
+      color: HINT_SELECTED_COLOR_RGB,
+    },
+    {
+      condition: (row, column) => notHighlightedColor(row, column),
+      color: NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+    {
+      condition: (row, column) => true,
+      color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
+    },
+  ]);
+
+  await sudokuBoard.hintFinish.click();
+
+  await sudokuBoard.isSudokuBoardHighlightedCorrectly([
+    { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
+  ]);
+
+  await sudokuBoard.cellHasNotes(row, column, stageFiveCellNotes);
+
+  // testing undo works at end of hint
+  await sudokuBoard.undo.click();
+  if (initialCellNotes === "0") {
+    await sudokuBoard.cellHasValue(row, column, initialCellNotes);
+  } else {
+    await sudokuBoard.cellHasNotes(row, column, initialCellNotes);
+  }
+};
 
 test.describe("board NAKED_SINGLE", () => {
   test.use({ gameToResume: NAKED_SINGLE_GAME });
