10137053becb8e03a05ade9b33bb71296e6def05
Galla <gallant.thomas@outlook.com>
2024-04-21 01:02:14 -0400

    Condensing logic even more

--- e2e/web/specs/hints.spec.ts
+++ e2e/web/specs/hints.spec.ts
@@ -12,6 +12,8 @@ import {
   HINT_SELECTED_COLOR_RGB,
   NOT_HIGHLIGHTED_COLOR_RGB,
 } from "../../../app/Styling/HighlightColors";
+import { sudokuStrategy } from "sudokuru";
+import { toTitle } from "../../../app/Components/Sudoku Board/sudoku";
 
 // todo test that board is unselected when entering hint mode
 
@@ -46,8 +48,9 @@ test.describe("board AMEND_NOTES", () => {
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
 
-    await amendNotesBaseTest(
+    await hintBaseTest(
       sudokuBoard,
+      "AMEND_NOTES",
       0,
       0,
       hintSelectedColor,
@@ -79,8 +82,9 @@ test.describe("board AMEND_NOTES", () => {
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
 
-    await amendNotesBaseTest(
+    await hintBaseTest(
       sudokuBoard,
+      "AMEND_NOTES",
       0,
       5,
       hintSelectedColor,
@@ -113,8 +117,9 @@ test.describe("board AMEND_NOTES", () => {
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
 
-    await amendNotesBaseTest(
+    await hintBaseTest(
       sudokuBoard,
+      "AMEND_NOTES",
       0,
       6,
       hintSelectedColor,
@@ -149,8 +154,9 @@ test.describe("board AMEND_NOTES", () => {
 
     const sudokuBoard = new SudokuBoardComponent(resumeGame);
 
-    await amendNotesBaseTest(
+    await hintBaseTest(
       sudokuBoard,
+      "AMEND_NOTES",
       0,
       8,
       hintSelectedColor,
@@ -162,8 +168,35 @@ test.describe("board AMEND_NOTES", () => {
   });
 });
 
+test.describe("board NAKED_SINGLE", () => {
+  test.use({ gameToResume: NAKED_SINGLE_GAME });
+  test("NAKED_SINGLE", async ({ resumeGame }) => {
+    const notHighlightedColor = (row: number, column: number) => {
+      return row <= 2 && column <= 2;
+    };
+
+    const hintSelectedColor = (row: number, column: number) => {
+      return row === 0 && column === 0;
+    };
+
+    const sudokuBoard = new SudokuBoardComponent(resumeGame);
+
+    await hintBaseTest(
+      sudokuBoard,
+      "NAKED_SINGLE",
+      0,
+      0,
+      hintSelectedColor,
+      notHighlightedColor,
+      { contentType: "notes", content: "1" },
+      { contentType: "notes", content: "1" },
+      { contentType: "value", content: "1" }
+    );
+  });
+});
+
 /**
- * This is a helper function for Amend Notes testing.
+ * This is a helper function for Hint testing.
  * This tests highlighting at relevant stages, hint undo and 'redo' functionality, and cell content of the hint stages.
  * @param sudokuBoard The playwright sudoku board page object.
  * @param row This is the row (0-8) of the target cell of the hint.
@@ -174,8 +207,9 @@ test.describe("board AMEND_NOTES", () => {
  * @param stageFourCellNotes This is an object containing the cell content and cell content type for the target cell for hint stage four.
  * @param stageFiveCellNotes This is an object containing the cell content and cell content type for the target cell for hint stage five.
  */
-const amendNotesBaseTest = async (
+const hintBaseTest = async (
   sudokuBoard: SudokuBoardComponent,
+  strategy: sudokuStrategy,
   row: number,
   column: number,
   hintSelectedColor: (row: number, column: number) => boolean,
@@ -186,10 +220,12 @@ const amendNotesBaseTest = async (
 ) => {
   await sudokuBoard.hint.click();
 
-  await sudokuBoard.sudokuBoardContainsText("Amend Notes");
+  await sudokuBoard.sudokuBoardContainsText(
+    getHintMessageForStage(1, strategy)
+  );
   await sudokuBoard.hintArrowRight.click();
   await sudokuBoard.sudokuBoardContainsText(
-    "Amend notes are when you reset a cell's notes to contain every nonconflicting number"
+    getHintMessageForStage(2, strategy)
   );
   await sudokuBoard.hintArrowRight.click();
 
@@ -317,84 +353,24 @@ const amendNotesBaseTest = async (
   );
 };
 
-test.describe("board NAKED_SINGLE", () => {
-  test.use({ gameToResume: NAKED_SINGLE_GAME });
-  test("NAKED_SINGLE", async ({ resumeGame }) => {
-    const sudokuBoard = new SudokuBoardComponent(resumeGame);
-    await sudokuBoard.hint.click();
-
-    await sudokuBoard.sudokuBoardContainsText("Naked Single");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.sudokuBoardContainsText(
-      "Naked singles are when you only have one number left as a possibility in a cell"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => row <= 2 && column <= 2,
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.sudokuBoardContainsText(
-      "The hint is located in this region"
-    );
-    await sudokuBoard.hintArrowRight.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => row === 0 && column === 0,
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => row <= 2 && column <= 2,
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasValue(0, 0, "1");
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      {
-        condition: (row, column) => row === 0 && column === 0,
-        color: HINT_SELECTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => row <= 2 && column <= 2,
-        color: NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-      {
-        condition: (row, column) => true,
-        color: HINT_NOT_HIGHLIGHTED_COLOR_RGB,
-      },
-    ]);
-
-    // testing undo logic
-    await sudokuBoard.hintArrowLeft.click();
-    await sudokuBoard.cellHasNotes(0, 0, "1");
-    await sudokuBoard.hintArrowRight.click();
-    await sudokuBoard.cellHasValue(0, 0, "1");
-
-    await sudokuBoard.hintFinish.click();
-
-    await sudokuBoard.isSudokuBoardHighlightedCorrectly([
-      { condition: (row, column) => true, color: NOT_HIGHLIGHTED_COLOR_RGB },
-    ]);
-
-    await sudokuBoard.cellHasValue(0, 0, "1");
-
-    // testing undo works at end of hint
-    await sudokuBoard.undo.click();
-    await sudokuBoard.cellHasNotes(0, 0, "1");
-  });
-});
+/**
+ * Given stage number and hint type, returns the expected hint message
+ * @param stage The stage of the hint to retrieve the hint message
+ * @param hintType The hint type to recieve the hint message for
+ * @returns A string with the hint message for the hint type and stage
+ */
+const getHintMessageForStage = (stage: number, hintType: sudokuStrategy) => {
+  if (stage === 1) {
+    return toTitle(hintType);
+  } else if (stage === 2) {
+    if (hintType === "AMEND_NOTES") {
+      return "Amend notes are when you reset a cell's notes to contain every nonconflicting number";
+    } else if (hintType === "NAKED_SINGLE") {
+      return "Naked singles are when you only have one number left as a possibility in a cell";
+    } else {
+      return "Could not find Hint Message";
+    }
+  } else {
+    return "Could not find Hint Message";
+  }
+};
