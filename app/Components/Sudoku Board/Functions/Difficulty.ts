14d13ac598aa61654b3585e4f9aebb6ee0f965f2
Gregory Travis <gregorytravis711@gmail.com>
2024-07-02 21:06:04 -0400

    Moved remaining difficulty functions to new Difficulty functions file

--- app/Components/Sudoku Board/Functions/Difficulty.ts
+++ app/Components/Sudoku Board/Functions/Difficulty.ts
@@ -1,7 +1,15 @@
+import { AMATEUR_PUZZLES } from "../../../Data/puzzles/amateur_puzzles";
+import { LAYMAN_PUZZLES } from "../../../Data/puzzles/layman_puzzles";
+import { NOVICE_PUZZLES } from "../../../Data/puzzles/novice_puzzles";
+import { TRAINEE_PUZZLES } from "../../../Data/puzzles/trainee_puzzles";
 import {
-  GameDifficulty,
-  GameDifficultyScore,
+  convertPuzzleToSudokuObject,
+  Puzzle,
+  SudokuObjectProps,
 } from "../../../Functions/LocalDatabase";
+
+export type GameDifficulty = "novice" | "trainee" | "amateur" | "layman";
+export type GameDifficultyScore = 10 | 20 | 30;
 
 /**
  * Calculates the difficulty score
@@ -81,3 +89,44 @@ export function calculateGameScore(
   const timeScore: number = calculateTimeScore(time);
   return difficultyScore + hintAndIncorrectCellsScore + timeScore;
 }
+
+/**
+ * Returns a random puzzle from an array of puzzles
+ * @param PUZZLES An array of puzzles
+ * @returns a random puzzle from an array of puzzles
+ */
+const retrieveRandomPuzzle = (PUZZLES: Puzzle[]): Puzzle => {
+  return PUZZLES[Math.floor(Math.random() * PUZZLES.length)];
+};
+
+/**
+ * This function takes in the requested difficulty and returns a puzzle matching the difficulty.
+ * @param difficulty The difficulty classification of the puzzle. (string)
+ * @returns A puzzle object that is readable by the Sudoku component.
+ */
+export const returnGameOfDifficulty = (
+  difficulty: GameDifficulty | "dev"
+): SudokuObjectProps => {
+  let puzzle;
+  switch (difficulty) {
+    // "dev" difficulty is a custom difficulty that always returns the same puzzle.
+    case "dev":
+      puzzle = NOVICE_PUZZLES[0];
+      difficulty = "novice";
+      break;
+    case "novice":
+      puzzle = retrieveRandomPuzzle(NOVICE_PUZZLES);
+      break;
+    case "trainee":
+      puzzle = retrieveRandomPuzzle(TRAINEE_PUZZLES);
+      break;
+    case "amateur":
+      puzzle = retrieveRandomPuzzle(AMATEUR_PUZZLES);
+      break;
+    case "layman":
+      puzzle = retrieveRandomPuzzle(LAYMAN_PUZZLES);
+      break;
+  }
+
+  return convertPuzzleToSudokuObject(puzzle, difficulty);
+};
