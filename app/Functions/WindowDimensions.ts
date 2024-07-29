669b7df42cd17276e24e629fc2d86f14c59b55b1
Galla <gallant.thomas@outlook.com>
2024-07-29 07:47:17 -0400

    Fixing header height issue

--- app/Functions/WindowDimensions.ts
+++ app/Functions/WindowDimensions.ts
@@ -1,16 +1,29 @@
 import { ScaledSize, useWindowDimensions } from "react-native";
-import { useHeaderHeight } from "@react-navigation/elements";
+import { getDefaultHeaderHeight } from "@react-navigation/elements";
+import {
+  useSafeAreaFrame,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
+
+// the useHeaderHook does equals 0 in nested stacks
+// https://github.com/react-navigation/react-navigation/issues/8505
+// https://stackoverflow.com/questions/68820636/react-navigation-6-custom-header-height
+// https://github.com/react-navigation/react-navigation/issues/10097#issuecomment-1131928669
 
 export const useMinWindowDimensions = (): number => {
   const size = useWindowDimensions();
-  const headerHeight = useHeaderHeight();
+  const frame = useSafeAreaFrame();
+  const insets = useSafeAreaInsets();
+  const headerHeight = getDefaultHeaderHeight(frame, false, insets.top);
   return Math.min(size.width, size.height - headerHeight);
 };
 
 export function useNewWindowDimensions(): ScaledSize {
   const size = useWindowDimensions();
   const clone = JSON.parse(JSON.stringify(size));
-  const headerHeight = useHeaderHeight();
+  const frame = useSafeAreaFrame();
+  const insets = useSafeAreaInsets();
+  const headerHeight = getDefaultHeaderHeight(frame, false, insets.top);
   clone.height = size.height - headerHeight;
   return clone;
 }
