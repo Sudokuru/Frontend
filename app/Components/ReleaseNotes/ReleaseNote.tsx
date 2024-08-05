e0b923455e332c2a732416460ab81f81ab662a4c
Galla <gallant.thomas@outlook.com>
2024-08-04 23:02:57 -0400

    Restructuring bulleted list to be formatted better

--- app/Components/ReleaseNotes/ReleaseNote.tsx
+++ app/Components/ReleaseNotes/ReleaseNote.tsx
@@ -13,27 +13,56 @@ export interface ReleaseNoteInterface {
 }
 
 export const ReleaseNote = (props: ReleaseNoteInterface, key: number) => {
-  const createArrayString = (items: string[]) => {
-    let itemString = "";
-    for (const item of items) {
-      itemString = itemString + "• " + item + "\n";
-    }
-    itemString = itemString.slice(0, -1); // remove last \n character
-    return itemString;
+  // https://stackoverflow.com/questions/40441877/react-native-bulleted-lists-using-flex-wrap
+  /**
+   * Takes in a string and creates a bullet component
+   * @param point a string for the bullet component
+   * @returns A JSX element of a bullet
+   */
+  const BulletComponent = (point: string) => {
+    // note the reason why the Text elements are wrapped in view components is to
+    // keep spacing even. Text components have different spacing based on character length otherwise,
+    // leading to variance in the spacing between bullet and first character.
+    return (
+      <View style={{ flexDirection: "row", paddingLeft: 20, maxWidth: 800 }}>
+        <View>
+          <Text style={{ width: 10, fontSize: 14 }}>•</Text>
+        </View>
+        <View style={{ flex: 1, flexWrap: "wrap" }}>
+          <Text style={{ fontSize: 14 }}>{point}</Text>
+        </View>
+      </View>
+    );
   };
 
-  let featureString = "None";
+  /**
+   * Takes in a list of strings and creates a bullet list component
+   * @param points A list of strings for the bulleted list component
+   * @returns A JSX element of a bullet list component
+   */
+  const BulletedListComponent = (points: string[]) => {
+    const list = [];
+    for (const point of points) {
+      list.push(BulletComponent(point));
+    }
+    return list;
+  };
+
+  let featureList = ["None"];
   if (props.features) {
-    featureString = createArrayString(props.features);
+    featureList = props.features;
   }
 
-  let bugFixString = "None";
+  const featureListComponent = BulletedListComponent(featureList);
+
+  let bugList = ["None"];
   if (props["bug fixes"]) {
-    bugFixString = createArrayString(props["bug fixes"]);
+    bugList = props["bug fixes"];
   }
 
-  const targetPlatformsString = createArrayString(props.targets);
-  const contributorsString = createArrayString(props.contributors);
+  const bugListComponent = BulletedListComponent(bugList);
+  const targetPlatformsString = BulletedListComponent(props.targets);
+  const contributorsString = BulletedListComponent(props.contributors);
 
   return (
     <View
@@ -42,7 +71,6 @@ export const ReleaseNote = (props: ReleaseNoteInterface, key: number) => {
         borderRadius: 20,
         padding: 10,
         marginBottom: 10,
-        alignItems: "stretch",
       }}
       key={key}
     >
@@ -55,21 +83,13 @@ export const ReleaseNote = (props: ReleaseNoteInterface, key: number) => {
           {props.summary}
         </Text>
         <Text style={{ fontSize: 20 }}>Features: </Text>
-        <Text style={{ paddingLeft: 20, maxWidth: 800, fontSize: 14 }}>
-          {featureString}
-        </Text>
+        {featureListComponent}
         <Text style={{ fontSize: 20 }}>Bug Fixes: </Text>
-        <Text style={{ paddingLeft: 20, maxWidth: 800, fontSize: 14 }}>
-          {bugFixString}
-        </Text>
+        {bugListComponent}
         <Text style={{ fontSize: 20 }}>Target Platforms: </Text>
-        <Text style={{ paddingLeft: 20, fontSize: 14 }}>
-          {targetPlatformsString}
-        </Text>
+        {targetPlatformsString}
         <Text style={{ fontSize: 20 }}>Contributors: </Text>
-        <Text style={{ paddingLeft: 20, fontSize: 14 }}>
-          {contributorsString}
-        </Text>
+        {contributorsString}
       </>
     </View>
   );
