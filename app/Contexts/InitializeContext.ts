0c77020ea6bacc085454aa13b32d15f90d2738f6
Galla <gallant.thomas@outlook.com>
2024-08-22 20:10:41 -0400

    refactoring name for state

--- app/Contexts/InitializeContext.ts
+++ app/Contexts/InitializeContext.ts
@@ -10,23 +10,25 @@ const InitializeContext = () => {
   const [darkThemeSetting, setDarkThemeSetting] = React.useState(true);
   const [currentPage, setCurrentPage] = React.useState("Landing");
   const [learnedLessons, setLearnedLessons] = React.useState(["NONE"]);
-  const [highlightIdenticalValuesSetting, setHighlightIdenticalValues] =
+  const [highlightIdenticalValuesSetting, setHighlightIdenticalValuesSetting] =
     React.useState(true);
-  const [highlightBoxSetting, setHighlightBox] = React.useState(true);
-  const [highlightRowSetting, setHighlightRow] = React.useState(true);
-  const [highlightColumnSetting, setHighlightColumn] = React.useState(true);
+  const [highlightBoxSetting, setHighlightBoxSetting] = React.useState(true);
+  const [highlightRowSetting, setHighlightRowSetting] = React.useState(true);
+  const [highlightColumnSetting, setHighlightColumnSetting] =
+    React.useState(true);
 
-  const [featurePreviewSetting, setFeaturePreview] = React.useState(false);
+  const [featurePreviewSetting, setFeaturePreviewSetting] =
+    React.useState(false);
 
   // set initial values of theme
   React.useEffect(() => {
     Profile.getProfile().then((data: profile) => {
       setDarkThemeSetting(data.theme);
-      setHighlightIdenticalValues(data.highlightIdenticalValues);
-      setHighlightBox(data.highlightBox);
-      setHighlightRow(data.highlightRow);
-      setHighlightColumn(data.highlightColumn);
-      setFeaturePreview(data.previewMode);
+      setHighlightIdenticalValuesSetting(data.highlightIdenticalValues);
+      setHighlightBoxSetting(data.highlightBox);
+      setHighlightRowSetting(data.highlightRow);
+      setHighlightColumnSetting(data.highlightColumn);
+      setFeaturePreviewSetting(data.previewMode);
     });
   }, []);
 
@@ -53,27 +55,27 @@ const InitializeContext = () => {
 
   const toggleHighlightIdenticalValues = React.useCallback(() => {
     Profile.setProfileValue("highlightIdenticalValues");
-    return setHighlightIdenticalValues(!highlightIdenticalValuesSetting);
+    return setHighlightIdenticalValuesSetting(!highlightIdenticalValuesSetting);
   }, [highlightIdenticalValuesSetting]);
 
   const toggleHighlightBox = React.useCallback(() => {
     Profile.setProfileValue("highlightBox");
-    return setHighlightBox(!highlightBoxSetting);
+    return setHighlightBoxSetting(!highlightBoxSetting);
   }, [highlightBoxSetting]);
 
   const toggleHighlightRow = React.useCallback(() => {
     Profile.setProfileValue("highlightRow");
-    return setHighlightRow(!highlightRowSetting);
+    return setHighlightRowSetting(!highlightRowSetting);
   }, [highlightRowSetting]);
 
   const toggleHighlightColumn = React.useCallback(() => {
     Profile.setProfileValue("highlightColumn");
-    return setHighlightColumn(!highlightColumnSetting);
+    return setHighlightColumnSetting(!highlightColumnSetting);
   }, [highlightColumnSetting]);
 
   const toggleFeaturePreview = React.useCallback(() => {
     Profile.setProfileValue("previewMode");
-    return setFeaturePreview(!featurePreviewSetting);
+    return setFeaturePreviewSetting(!featurePreviewSetting);
   }, [featurePreviewSetting]);
 
   const preferences = React.useMemo(
