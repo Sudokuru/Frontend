0b279feceac0b88abd2b4d933c80a405d53071ce
Galla <gallant.thomas@outlook.com>
2024-08-24 08:39:25 -0400

    Fixing issues with sizing on mobile

--- app/Components/Profile/ProfileToggle.tsx
+++ app/Components/Profile/ProfileToggle.tsx
@@ -4,35 +4,38 @@ import { Switch, Text } from "react-native-paper";
 
 const ProfileToggle = (props: {
   name: string;
-  fontSize: number;
+  fontSize?: number;
   value: boolean;
   valueToggle: () => void;
   testIdPrefix: string;
 }) => {
+  let fontSize = props.fontSize;
+  if (!props.fontSize) {
+    fontSize = 25;
+  }
+
   return (
-    <View style={{ marginBottom: 10, flexDirection: "row" }}>
-      <Text style={{ fontSize: props.fontSize, color: "#025E73" }}>
+    <View
+      style={{
+        marginBottom: 10,
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <Text style={{ fontSize: fontSize, color: "#025E73" }}>
         {props.name}:{" "}
       </Text>
-      <View
-        style={{
-          justifyContent: "flex-end",
-          flexDirection: "row",
-          flex: 1,
-        }}
-      >
-        <Switch
-          color={"#025E73"}
-          value={props.value}
-          onValueChange={props.valueToggle}
-          testID={
-            props.value
-              ? props.testIdPrefix + "Enabled"
-              : props.testIdPrefix + "Disabled"
-          }
-          style={{ alignSelf: "center", flexDirection: "column" }}
-        />
-      </View>
+      <Switch
+        color={"#025E73"}
+        value={props.value}
+        onValueChange={props.valueToggle}
+        testID={
+          props.value
+            ? props.testIdPrefix + "Enabled"
+            : props.testIdPrefix + "Disabled"
+        }
+        style={{ alignSelf: "center" }}
+      />
     </View>
   );
 };
--- app/Pages/ProfilePage.tsx
+++ app/Pages/ProfilePage.tsx
@@ -28,84 +28,73 @@ const ProfilePage = () => {
   } = React.useContext(PreferencesContext);
 
   return (
-    <ScrollView>
-      <View
+    <ScrollView
+      contentContainerStyle={{ justifyContent: "center", alignItems: "center" }}
+    >
+      <Text
         style={{
-          alignItems: "center",
-          justifyContent: "center",
-          marginVertical: 30,
+          fontSize: 40,
+          color: theme.colors.primary,
+          fontWeight: "bold",
+          marginBottom: 10,
         }}
       >
-        <Text
-          style={{
-            fontSize: reSize / 20,
-            color: theme.colors.primary,
-            fontWeight: "bold",
-            marginBottom: 10,
-          }}
-        >
-          Profile
-        </Text>
-        <View
-          style={{ backgroundColor: "#fff", borderRadius: 10, padding: 20 }}
-        >
-          <View style={{ marginBottom: 10 }}>
-            <Text style={{ fontSize: reSize / 22, color: "#025E73" }}>
-              Strategies Learned:
-            </Text>
-            <Text
-              style={{
-                fontSize: reSize / 40,
-                fontWeight: "bold",
-                color: "#D9A05B",
-              }}
-            >
-              {formatLessonNameArray(learnedLessons)}
-            </Text>
-          </View>
-          <ProfileToggle
-            name="Theme"
-            fontSize={reSize / 22}
-            value={darkThemeSetting}
-            valueToggle={toggleTheme}
-            testIdPrefix="DarkTheme"
-          ></ProfileToggle>
-          <ProfileToggle
-            name="Highlight Identical Values"
-            fontSize={reSize / 22}
-            value={highlightIdenticalValuesSetting}
-            valueToggle={toggleHighlightIdenticalValues}
-            testIdPrefix="HighlightIdenticalValues"
-          ></ProfileToggle>
-          <ProfileToggle
-            name="Highlight Box"
-            fontSize={reSize / 22}
-            value={highlightBoxSetting}
-            valueToggle={toggleHighlightBox}
-            testIdPrefix="HighlightBox"
-          ></ProfileToggle>
-          <ProfileToggle
-            name="Highlight Row"
-            fontSize={reSize / 22}
-            value={highlightRowSetting}
-            valueToggle={toggleHighlightRow}
-            testIdPrefix="HighlightRow"
-          ></ProfileToggle>
-          <ProfileToggle
-            name="Highlight Column"
-            fontSize={reSize / 22}
-            value={highlightColumnSetting}
-            valueToggle={toggleHighlightColumn}
-            testIdPrefix="HighlightColumn"
-          ></ProfileToggle>
-          <ProfileToggle
-            name="Feature Preview"
-            fontSize={reSize / 22}
-            value={featurePreviewSetting}
-            valueToggle={toggleFeaturePreview}
-            testIdPrefix="FeaturePreview"
-          ></ProfileToggle>
+        Profile
+      </Text>
+      <View style={{ backgroundColor: "#fff", borderRadius: 10, padding: 20 }}>
+        <View style={{ marginBottom: 10 }}>
+          <Text style={{ fontSize: 25, color: "#025E73" }}>
+            Strategies Learned:
+          </Text>
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "#D9A05B",
+            }}
+          >
+            {formatLessonNameArray(learnedLessons)}
+          </Text>
         </View>
+        <ProfileToggle
+          name="Theme"
+          value={darkThemeSetting}
+          valueToggle={toggleTheme}
+          testIdPrefix="DarkTheme"
+        ></ProfileToggle>
+        <Text style={{ fontSize: 25, color: "#025E73", marginBottom: 10 }}>
+          Highlight
+        </Text>
+        <ProfileToggle
+          name="  Identical Values"
+          value={highlightIdenticalValuesSetting}
+          valueToggle={toggleHighlightIdenticalValues}
+          testIdPrefix="HighlightIdenticalValues"
+        ></ProfileToggle>
+        <ProfileToggle
+          name="  Box"
+          value={highlightBoxSetting}
+          valueToggle={toggleHighlightBox}
+          testIdPrefix="HighlightBox"
+        ></ProfileToggle>
+        <ProfileToggle
+          name="  Row"
+          value={highlightRowSetting}
+          valueToggle={toggleHighlightRow}
+          testIdPrefix="HighlightRow"
+        ></ProfileToggle>
+        <ProfileToggle
+          name="  Column"
+          value={highlightColumnSetting}
+          valueToggle={toggleHighlightColumn}
+          testIdPrefix="HighlightColumn"
+        ></ProfileToggle>
+        <ProfileToggle
+          name="Feature Preview"
+          value={featurePreviewSetting}
+          valueToggle={toggleFeaturePreview}
+          testIdPrefix="FeaturePreview"
+        ></ProfileToggle>
       </View>
     </ScrollView>
   );
