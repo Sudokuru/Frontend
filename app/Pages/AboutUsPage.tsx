3793d6084474a60bd2f76afc2eb058bc0c8e224a
Gregory Travis <gregorytravis711@gmail.com>
2024-09-29 21:12:45 -0400

    Styled about us page title using theme

--- app/Pages/AboutUsPage.tsx
+++ app/Pages/AboutUsPage.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { ScrollView } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, useTheme } from "react-native-paper";
 
 const AboutUsPage = () => {
+  const theme = useTheme();
+
   return (
     <ScrollView>
-      <Text>About Us</Text>
+      <Text
+        style={{
+          color: theme.colors.primary,
+          fontSize: 50,
+          lineHeight: 50,
+          fontWeight: "bold",
+          alignSelf: "center",
+        }}
+      >
+        About Us
+      </Text>
     </ScrollView>
   );
 };
