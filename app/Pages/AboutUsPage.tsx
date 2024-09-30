fa1f63e3a553018bc332ad2c5de50c7030542195
Gregory Travis <gregorytravis711@gmail.com>
2024-09-29 21:00:43 -0400

    Started About Us page

---
+++ app/Pages/AboutUsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { Text } from "react-native-paper";
+
+const AboutUsPage = () => {
+  return (
+    <ScrollView>
+      <Text>About Us</Text>
+    </ScrollView>
+  );
+};
+
+export default AboutUsPage;
