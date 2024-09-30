9c741aa935a2f0afbc3b31378cb2a304a56981f6
Gregory Travis <gregorytravis711@gmail.com>
2024-09-29 21:36:42 -0400

    Added history section to about us page and updated styling

--- app/Pages/AboutUsPage.tsx
+++ app/Pages/AboutUsPage.tsx
@@ -18,7 +18,7 @@ const AboutUsPage = () => {
       >
         About Us
       </Text>
-      <View style={{ marginLeft: "1%" }}>
+      <View style={{ marginHorizontal: "1%" }}>
         <Text variant="headlineSmall">Mission</Text>
         <Text variant="bodyLarge">
           {/* Note: The following text is duplicated in the README.md file */}
@@ -29,6 +29,14 @@ const AboutUsPage = () => {
           be free, well-documented, modern, and interoperable, allowing
           developers to easily incorporate them into their own Sudoku-related
           projects.
+        </Text>
+        <Text variant="headlineSmall">History</Text>
+        <Text variant="bodyLarge">
+          Sudokuru was founded in 2022 by a group of computer science students
+          at the University of Central Florida for a senior design project.
+          Members of this original team have continued developing the app after
+          graduation with a current goal of launching the official production
+          website in early 2025 and on mobile appstores later that year.
         </Text>
       </View>
     </ScrollView>
