879f291a85906fd4d854c3d87505674181d8f2d8
Gregory Travis <gregorytravis711@gmail.com>
2024-10-10 18:01:21 -0400

    Adding to about us page testing

--- e2e/web/page/aboutus.page.ts
+++ e2e/web/page/aboutus.page.ts
@@ -1,9 +1,15 @@
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export class AboutUsPage {
   readonly page: Page;
+  readonly title: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.title = page.getByText("About Us");
+  }
+
+  async aboutUsPageIsRendered() {
+    await expect(this.title).toBeInViewport({ ratio: 1 });
   }
 }
