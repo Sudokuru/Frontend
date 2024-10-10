3a893acec36d1135a1a46722cc505d27717169e1
Gregory Travis <gregorytravis711@gmail.com>
2024-10-10 18:43:23 -0400

    Added to testing infra

--- e2e/web/page/aboutus.page.ts
+++ e2e/web/page/aboutus.page.ts
@@ -3,10 +3,24 @@ import { Locator, Page, expect } from "@playwright/test";
 export class AboutUsPage {
   readonly page: Page;
   readonly mission: Locator;
+  readonly teamMembers: Locator[];
+  readonly teamMemberButtons: Locator[];
 
   constructor(page: Page) {
     this.page = page;
     this.mission = page.getByText("Mission");
+    const teamMemberNames = [
+      "Greg",
+      "Thomas",
+      "Daniel",
+      "Arthur",
+      "Dahlia",
+      "Min",
+    ];
+    this.teamMembers = teamMemberNames.map((name) => page.getByTestId(name));
+    this.teamMemberButtons = teamMemberNames.map((name) =>
+      page.getByTestId("button-" + name)
+    );
   }
 
   async aboutUsPageIsRendered() {
