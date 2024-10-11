24f9b7f2825cf7c6b1e51255ecf7537701dfc6f3
Gregory Travis <gregorytravis711@gmail.com>
2024-10-10 20:22:35 -0400

    Reworking tests

--- e2e/web/page/aboutus.page.ts
+++ e2e/web/page/aboutus.page.ts
@@ -3,27 +3,17 @@ import { Locator, Page, expect } from "@playwright/test";
 export class AboutUsPage {
   readonly page: Page;
   readonly mission: Locator;
-  readonly teamMembers: Locator[];
-  readonly teamMemberButtons: Locator[];
 
   constructor(page: Page) {
     this.page = page;
     this.mission = page.getByText("Mission");
-    const teamMemberNames = [
-      "Greg",
-      "Thomas",
-      "Daniel",
-      "Arthur",
-      "Dahlia",
-      "Min",
-    ];
-    this.teamMembers = teamMemberNames.map((name) => page.getByTestId(name));
-    this.teamMemberButtons = teamMemberNames.map((name) =>
-      page.getByTestId("button-" + name)
-    );
   }
 
   async aboutUsPageIsRendered() {
     await expect(this.mission).toBeInViewport({ ratio: 1 });
+  }
+
+  async teamMemberCardIsVisible(name: string) {
+    await expect(this.page.getByTestId(name)).toBeInViewport({ ratio: 1 });
   }
 }
--- e2e/web/specs/aboutus.spec.ts
+++ e2e/web/specs/aboutus.spec.ts
@@ -1,12 +1,13 @@
 import { expect } from "@playwright/test";
 import { test } from "../fixture";
 import { AboutUsPage } from "../page/aboutus.page";
+import { teamMembers } from "../../../app/Data/members";
 
 test.describe("about us page", () => {
-  test("team member text are in cards", async ({ aboutUs }) => {
+  test("team member cards have text and buttons work", async ({ aboutUs }) => {
     const aboutUsPage = new AboutUsPage(aboutUs);
-    for (const teamMember of aboutUsPage.teamMembers) {
-      await expect(teamMember.getByText("Active")).toBeInViewport({ ratio: 1 });
+    for (const member of teamMembers) {
+      await aboutUsPage.teamMemberCardIsVisible(member.name);
     }
   });
 });
