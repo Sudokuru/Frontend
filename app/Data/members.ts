4b1a516258261e3682dd5cd4e82402118fba7354
Gregory Travis <gregorytravis711@gmail.com>
2024-10-10 20:20:11 -0400

    Refactored team member data into its own file

---
+++ app/Data/members.ts
@@ -0,0 +1,41 @@
+interface teamMember {
+  name: string;
+  github: string;
+  active: string;
+  specialty?: string;
+}
+
+export const teamMembers: teamMember[] = [
+  {
+    name: "Greg",
+    github: "https://github.com/Gregory711",
+    active: "Founding (Fall 2022) - Present",
+    specialty: "Sudoku Solving Algorithms",
+  },
+  {
+    name: "Thomas",
+    github: "https://github.com/Thomas-Gallant",
+    active: "Founding (Fall 2022) - Present",
+    specialty: "Continuous Integration & Deployment",
+  },
+  {
+    name: "Daniel",
+    github: "https://github.com/danielcosentino",
+    active: "Founding (Fall 2022) - End of Spring 2023",
+  },
+  {
+    name: "Arthur",
+    github: "https://github.com/salmoncore",
+    active: "Founding (Fall 2022) - End of Spring 2023",
+  },
+  {
+    name: "Dahlia",
+    github: "https://github.com/DahliaL",
+    active: "Founding (Fall 2022) - End of Spring 2023",
+  },
+  {
+    name: "Min",
+    github: "https://github.com/Mink234",
+    active: "Founding (Fall 2022) - End of Spring 2023",
+  },
+];
--- app/Pages/AboutUsPage.tsx
+++ app/Pages/AboutUsPage.tsx
@@ -7,53 +7,13 @@ import {
   CARD_WIDTH,
 } from "../Components/Home/Cards";
 import { useNewWindowDimensions } from "../Functions/WindowDimensions";
-
-interface teamMember {
-  name: string;
-  github: string;
-  active: string;
-  specialty?: string;
-}
+import { teamMembers } from "../Data/members";
 
 const AboutUsPage = () => {
   const windowSize = useNewWindowDimensions();
 
   const theme = useTheme();
 
-  const teamMembers: teamMember[] = [
-    {
-      name: "Greg",
-      github: "https://github.com/Gregory711",
-      active: "Founding (Fall 2022) - Present",
-      specialty: "Sudoku Solving Algorithms",
-    },
-    {
-      name: "Thomas",
-      github: "https://github.com/Thomas-Gallant",
-      active: "Founding (Fall 2022) - Present",
-      specialty: "Continuous Integration & Deployment",
-    },
-    {
-      name: "Daniel",
-      github: "https://github.com/danielcosentino",
-      active: "Founding (Fall 2022) - End of Spring 2023",
-    },
-    {
-      name: "Arthur",
-      github: "https://github.com/salmoncore",
-      active: "Founding (Fall 2022) - End of Spring 2023",
-    },
-    {
-      name: "Dahlia",
-      github: "https://github.com/DahliaL",
-      active: "Founding (Fall 2022) - End of Spring 2023",
-    },
-    {
-      name: "Min",
-      github: "https://github.com/Mink234",
-      active: "Founding (Fall 2022) - End of Spring 2023",
-    },
-  ];
   let teamCards = [];
   let subArray = [];
   let columnCount: number = calculateCardsPerRow(
