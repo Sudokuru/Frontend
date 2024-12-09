ba054bde4009b0d45f89cfa0974d1d4e349477ed
Galla <gallant.thomas@outlook.com>
2024-12-08 20:10:31 -0500

    fixing the script

--- .github/scripts/get-current-date.js
+++ .github/scripts/get-current-date.js
@@ -15,11 +15,12 @@ function getDaySuffix(day) {
       return "th";
   }
 }
+const utcDate = new Date().toLocaleString("en-US", { timeZone: 'America/New_York' });
 
-const date = new Date(new Date().toLocaleString("en-US", { timeZone: 'America/New_York' }));
+const date = new Date(utcDate);
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const day = date.getDate();
-console.log(date.toLocaleString('en-US', { timeZone: 'America/New_York', timeZoneName: 'short' }));
+console.log(utcDate);
 const suffix = getDaySuffix(day);
 const formattedDate = `${monthNames[date.getMonth()]} ${day}${suffix}, ${date.getFullYear()}`;
 await $`echo "DATE=${formattedDate}" >> $GITHUB_OUTPUT`;
