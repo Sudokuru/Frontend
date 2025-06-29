f24f7ec6dd650160d08eccc767548e3387d92e96
Galla <gallant.thomas@outlook.com>
2025-06-28 22:07:59 -0400

    creating isEqual function

--- sudokuru/app/Components/SudokuBoard/Drill/Functions/CellFunctions.ts
+++ sudokuru/app/Components/SudokuBoard/Drill/Functions/CellFunctions.ts
@@ -1,4 +1,4 @@
-import { BoardObjectProps } from "../../../../Functions/LocalDatabase";
+import { DrillObjectProps } from "../../../../Functions/LocalDatabase";
 
 /**
  * Checks if a given cell in the puzzle has a conflict with the solution.
@@ -9,13 +9,21 @@ import { BoardObjectProps } from "../../../../Functions/LocalDatabase";
  * @returns True if the cell's entry is incorrect; false otherwise.
  */
 export const doesCellHaveConflict = (
-  sudokuBoard: BoardObjectProps,
+  sudokuBoard: DrillObjectProps,
   r: number,
   c: number,
 ): boolean => {
   // todo there is an issue with this, will immediately show the user what cell is incorrect
   // todo need to do some sort of check to see if its not equal the initial state or the solution
-  return !(
-    sudokuBoard.puzzleState[r][c].entry === sudokuBoard.puzzleSolution[r][c]
+  return !isEqual(
+    sudokuBoard.puzzleState[r][c].entry,
+    sudokuBoard.puzzleSolution[r][c].entry,
   );
+};
+
+const isEqual = (a: any, b: any) => {
+  if (a === b) return true;
+  if (!Array.isArray(a) || !Array.isArray(b)) return false;
+  if (a.length !== b.length) return false;
+  return a.every((val, i) => val === b[i]);
 };
