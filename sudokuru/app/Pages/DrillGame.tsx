300e3b8318f3a0e2cd4225559a3de3401312d3e6
Galla <gallant.thomas@outlook.com>
2025-11-08 13:45:28 -0500

    cleaning up DrillGame page

--- sudokuru/app/Pages/DrillGame.tsx
+++ sudokuru/app/Pages/DrillGame.tsx
@@ -8,24 +8,31 @@ import { Text } from "react-native-paper";
 import SudokuBoard from "../Components/SudokuBoard/SudokuBoard";
 
 const Drill = (props: any) => {
-  let strategy = props.route.params
+  const strategy = props.route.params
     ? props.route.params.params
     : "no props.route.params in DrillPage";
 
-  let action = props.route.params
+  const action = props.route.params
     ? props.route.params.action
     : "no props.route.action in DrillPage";
-
-  strategy = strategy;
-  action = action;
-  const navigation: any = useNavigation();
 
   const isFocused = useIsFocused();
   if (!isFocused) return <Text>Error Loading Page</Text>;
 
   return (
-    <View style={homeScreenStyles.home}>
-      <View style={styles.statisticsTitle}>
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         {/* The game now required the info about it to be rendered, which is given in generateGame() */}
         <SudokuBoard type="drill" action={action} strategy={strategy} />
         <StatusBar style="auto" />
@@ -33,20 +40,5 @@ const Drill = (props: any) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  statisticsTitle: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const homeScreenStyles = StyleSheet.create({
-  home: {
-    display: "flex",
-    flexDirection: "row",
-  },
-});
 
 export default Drill;
