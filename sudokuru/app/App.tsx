cdc853107932cc0602dc0a05677bddbc302e4d0f
Galla <gallant.thomas@outlook.com>
2026-02-21 23:45:48 -0500

    saving progress

--- sudokuru/app/App.tsx
+++ sudokuru/app/App.tsx
@@ -1,11 +1,24 @@
 import "@expo/metro-runtime"; // web fast refresh for development
 import "react-native-gesture-handler"; // This needs to be at top to work
 import * as React from "react";
+import { Platform } from "react-native";
 import { PreferencesContext } from "./Contexts/PreferencesContext";
 import InitializeContext from "./Contexts/InitializeContext";
 import DrawerNavigator from "./Navigation/DrawerNavigator";
 import { registerRootComponent } from "expo";
-import { ThemeProvider } from "./Contexts/ThemeContext";
+import { ThemeProvider, useTheme } from "./Contexts/ThemeContext";
+
+function AppContent() {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    if (Platform.OS === "web") {
+      document.body.style.backgroundColor = theme.colors.bg;
+    }
+  }, [theme.colors.bg]);
+
+  return <DrawerNavigator />;
+}
 
 function App() {
   const { preferences } = InitializeContext();
@@ -13,7 +26,7 @@ function App() {
   return (
     <PreferencesContext.Provider value={preferences}>
       <ThemeProvider>
-        <DrawerNavigator />
+        <AppContent />
       </ThemeProvider>
     </PreferencesContext.Provider>
   );
