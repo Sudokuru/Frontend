9e9248e872a4f1ec9b7eacc32807f5114bc5badf
Galla <gallant.thomas@outlook.com>
2026-07-25 17:44:28 -0400

    Updating channel version and updating npm version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix development environment for Frontend";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     utils.url = "github:numtide/flake-utils";
   };
 
@@ -21,7 +21,7 @@
             git
             jq
             pre-commit
-            nodejs_24
+            nodejs_26
             bun
             cargo
             rustc
@@ -29,7 +29,7 @@
           ];
 
           shellHook = ''
-            echo "⚡ Pinned NixOS 25.11 Development Environment Activated ⚡"
+            echo "⚡ Pinned NixOS 26.05 Development Environment Activated ⚡"
             export PROJECT_ROOT=$(pwd)
             pre-commit install
             stools() {
